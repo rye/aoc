d18581f9e34daccd73578c239ed5d0fb8e7cdeb7
Kristofer Rye <kristofer.rye@gmail.com>
2019-11-30 23:38:02 -0600

    Add day01 rapid solution
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ day01/01.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+lines = open('input.txt', 'rb') do |io|
+	io.read
+end
+
+components = lines.split.map(&:to_f)
+
+fuel_requirements = components.map do |component|
+	(component / 3).floor - 2.0
+end
+
+fuel_sum = fuel_requirements.inject(:+)
+
+puts "Part 1: #{fuel_sum.to_i}"
+
+new_fuel_requirements = components.map do |component|
+	component_fuel_rqmt = (component / 3.0).floor - 2.0
+
+	chunk = [component_fuel_rqmt]
+
+	until (n = (chunk.last / 3.0).floor - 2.0) < 0
+		chunk << n
+	end
+
+	chunk.inject(:+)
+end
+
+total = new_fuel_requirements.inject(:+)
+
+puts "Part 2: #{total.to_i}"
+
