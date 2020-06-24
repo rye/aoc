d7c5581174f46cf9046a5e02d97e4a6330eb7f25
Kristofer Rye <kristofer.rye@gmail.com>
2020-06-24 13:29:33 -0500

    d2019-02: Read in the program
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2019/d2019-02/src/main.rs
+++ 2019/d2019-02/src/main.rs
@@ -3,5 +3,17 @@ mod one {}
 mod two {}
 
 fn main() {
-	println!("Hello, world!");
+	use std::io::BufRead;
+
+	let program: Vec<i32> = std::io::stdin()
+		.lock()
+		.lines()
+		.flat_map(|line: Result<String, std::io::Error>| -> Vec<i32> {
+			line
+				.unwrap()
+				.split(',')
+				.map(|n| n.parse::<i32>().unwrap())
+				.collect()
+		})
+		.collect();
 }
