838f5041d25f79c2b7dc56fd180fd3ecb79cb4a2
Kristofer Rye <kristofer.rye@gmail.com>
2020-06-24 12:16:58 -0500

    d2019-01: Implement input parsing
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2019/d2019-01/src/main.rs
+++ 2019/d2019-01/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-	println!("Hello, world!");
+	use std::io::BufRead;
+
+	// Collect the input, which is a long list of positive integer weights of modules.
+	let module_weights: Vec<u32> = std::io::stdin()
+		.lock()
+		.lines()
+		.map(|line| line.unwrap().parse().unwrap())
+		.collect();
 }
