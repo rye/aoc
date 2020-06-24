40a5b207f4d1075fe2ca83875785fa2134db3436
Kristofer Rye <kristofer.rye@gmail.com>
2020-06-24 12:22:04 -0500

    d2019-01: Complete Part Two logic, with comments
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2019/d2019-01/src/main.rs
+++ 2019/d2019-01/src/main.rs
@@ -26,7 +26,30 @@ mod one {
 
 mod two {
 	pub fn fuel_required_for_module(mass: u32) -> u32 {
-		todo!()
+		// Compute the required fuel (if any) for a given mass.
+		let fuel_for_mass = |mass: u32| (mass / 3).checked_sub(2);
+
+		// A running total.
+		let mut total: u32 = 0;
+
+		// Copy and mutify the mass variable.
+		let mut mass = mass;
+
+		// This loop runs until no more fuel is required for the remaining unaccounted mass.
+		loop {
+			// If we need some more fuel for the mass,
+			if let Some(part) = fuel_for_mass(mass) {
+				// add the required fuel to the total.
+				total += part;
+				// Now we only have to compute for the mass we added.
+				mass = part;
+			} else {
+				// If we didn't need any additional fuel, we're done.
+				break;
+			}
+		}
+
+		total
 	}
 
 	#[test]
