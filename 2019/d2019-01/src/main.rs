abb00d873755676fcb0c8b559db0fa7aae046d4e
Kristofer Rye <kristofer.rye@gmail.com>
2020-06-24 12:17:49 -0500

    d2019-01: Add structure for Part One logic
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2019/d2019-01/src/main.rs
+++ 2019/d2019-01/src/main.rs
@@ -1,3 +1,29 @@
+mod one {
+	pub fn fuel_required_for_module(mass: u32) -> u32 {
+		todo!()
+	}
+
+	#[test]
+	fn fuel_12() {
+		assert_eq!(fuel_required_for_module(12), 2);
+	}
+
+	#[test]
+	fn fuel_14() {
+		assert_eq!(fuel_required_for_module(14), 2);
+	}
+
+	#[test]
+	fn fuel_1969() {
+		assert_eq!(fuel_required_for_module(1969), 654);
+	}
+
+	#[test]
+	fn fuel_100756() {
+		assert_eq!(fuel_required_for_module(100756), 33583);
+	}
+}
+
 fn main() {
 	use std::io::BufRead;
 
@@ -7,4 +33,15 @@ fn main() {
 		.lines()
 		.map(|line| line.unwrap().parse().unwrap())
 		.collect();
+
+	// Part One: Compute the sum of the direct fuel requirements for each module.
+	{
+		let mut fuel_weight_sum = 0;
+
+		for module_weight in &module_weights {
+			fuel_weight_sum += one::fuel_required_for_module(*module_weight);
+		}
+
+		println!("Part One: {}", fuel_weight_sum);
+	}
 }
