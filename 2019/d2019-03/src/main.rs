5d7bf82f3ce8f1df63e94ee7a4d8078a0f2513d4
Kristofer Rye <kristofer.rye@gmail.com>
2020-07-03 20:16:37 -0500

    d2019-03: Implement and finish day 3
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2019/d2019-03/src/main.rs
+++ 2019/d2019-03/src/main.rs
@@ -1,7 +1,12 @@
+use std::io::BufRead;
+
 mod one {
 	use core::borrow::Borrow;
-	use core::ops::Mul;
+	use core::convert::{TryFrom, TryInto};
 	use core::ops::Add;
+	use core::ops::Mul;
+	use core::str::FromStr;
+	use std::collections::HashSet;
 
 	#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 	pub struct Vec2d(pub i32, pub i32);
@@ -61,8 +66,118 @@ mod one {
 		}
 	}
 
+	#[derive(Debug)]
+	pub enum SegmentParseError {
+		MissingComponent,
+		InvalidDirection,
+	}
+
+	#[derive(Debug)]
+	pub struct Segment {
+		direction: Direction,
+		distance: i32,
+	}
+
+	#[allow(dead_code)]
+	impl Segment {
+		pub fn direction(&self) -> &Direction {
+			&self.direction
+		}
+
+		pub fn distance(&self) -> &i32 {
+			&self.distance
+		}
+	}
+
+	impl FromStr for Segment {
+		type Err = SegmentParseError;
+
+		fn from_str(s: &str) -> Result<Self, Self::Err> {
+			let direction: char = s
+				.chars()
+				.nth(0)
+				.ok_or(SegmentParseError::MissingComponent)?;
+			let distance: i32 = s[1..]
+				.parse()
+				.or(Err(SegmentParseError::MissingComponent))?;
+
+			let direction: Direction = direction.try_into()?;
+
+			Ok(Segment {
+				direction,
+				distance,
+			})
+		}
+	}
+
+	#[derive(Clone, Debug)]
+	pub struct Wire {
+		points: Vec<Vec2d>,
+	}
+
+	impl Wire {
+		pub fn intersection(&self, other: &Wire) -> Vec<Vec2d> {
+			let self_points: HashSet<&Vec2d> = self.points.iter().collect();
+			let other_points: HashSet<&Vec2d> = other.points.iter().collect();
+
+			self_points
+				.intersection(&other_points)
+				.copied()
+				.copied()
+				.collect()
+		}
+
+		pub fn signal_distance_to(&self, point: &Vec2d) -> Option<i32> {
+			self
+				.points
+				.iter()
+				.position(|self_point| self_point == point)
+				.map(|us| us.try_into().unwrap())
+		}
+	}
+
+	impl From<Vec<Segment>> for Wire {
+		fn from(segments: Vec<Segment>) -> Wire {
+			let origin: Vec2d = Vec2d(0, 0);
+			let mut points: Vec<Vec2d> = Vec::new();
+			points.push(origin);
+
+			let mut last: Vec2d = origin;
+
+			for segment in segments {
+				let direction: Direction = segment.direction;
+				let direction: Vec2d = direction.into();
+
+				for i in 1..=segment.distance {
+					points.push(last + direction * i);
+				}
+
+				last = last + direction * segment.distance;
+			}
+
+			Wire { points }
+		}
+	}
+
 	#[cfg(test)]
-	mod tests {}
+	mod tests {
+		use super::*;
+
+		#[test]
+		fn can_parse_segment() {
+			let segment: Segment = "D42".parse().unwrap();
+			assert_eq!(segment.direction(), &Direction::Down);
+			assert_eq!(segment.distance(), &42_i32);
+		}
+
+		#[test]
+		fn can_add_vecs() {
+			let v1: Vec2d = Vec2d(0, 0);
+			let v2: Vec2d = Vec2d(3, 5);
+
+			assert_eq!(v1 + v2, Vec2d(3, 5))
+		}
+	}
 }
 
 mod two {
@@ -71,5 +186,57 @@ mod two {
 }
 
 fn main() {
-	println!("Hello, world!");
+	use core::cmp::Ordering;
+	use one::{Segment, Vec2d, Wire};
+
+	let wires: Vec<Wire> = std::io::stdin()
+		.lock()
+		.lines()
+		.filter_map(Result::ok)
+		.map(|s: String| -> Vec<Segment> {
+			s.split(',')
+				.map(|k: &str| k.parse::<Segment>())
+				.map(Result::unwrap)
+				.collect()
+		})
+		.map(Into::into)
+		.collect();
+
+	let a = &wires[0];
+	let b = &wires[1];
+	let intersections: Vec<Vec2d> = a.intersection(b);
+
+	let origin: Vec2d = Vec2d(0, 0);
+
+	let mut intersection_distances: Vec<(Vec2d, i32)> = intersections
+		.iter()
+		.map(|intersection: &Vec2d| -> (Vec2d, i32) {
+			(
+				intersection.clone(),
+				intersection.manhattan_distance(&origin),
+			)
+		})
+		.collect();
+
+	intersection_distances
+		.sort_by(|a: &(Vec2d, i32), b: &(Vec2d, i32)| -> Ordering { a.1.cmp(&b.1) });
+
+	println!("Part 1: {:?}", intersection_distances[1].1);
+
+	let mut intersection_signal_distances: Vec<(Vec2d, i32, i32, i32)> = intersections
+		.iter()
+		.map(|intersection: &Vec2d| {
+			let a: i32 = a
+				.signal_distance_to(intersection)
+				.expect("intersection not on wire a?");
+			let b: i32 = b
+				.signal_distance_to(intersection)
+				.expect("intersection not on wire b?");
+			(intersection.clone(), a, b, a + b)
+		})
+		.collect();
+
+	intersection_signal_distances.sort_by(|a, b| a.3.partial_cmp(&b.3).unwrap());
+
+	println!("Part 2: {:?}", intersection_signal_distances[1].3);
 }
