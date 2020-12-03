91c7421d2b25f349346b8c345e5c54b8719c2ac1
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-02 21:13:32 -0600

    d2019-05: Add initial stub
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2019/d2019-05/src/main.rs
+++ 2019/d2019-05/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-	println!("Hello, world!");
+	use std::io::BufRead;
+
+	let program: Vec<i32> = std::io::stdin()
+		.lock()
+		.lines()
+		.flat_map(|line: Result<String, std::io::Error>| -> Vec<i32> {
+			line
+				.unwrap()
+				.split(',')
+				.map(|n| n.parse::<i32>().unwrap())
+				.collect()
+		})
+		.collect();
+
+	// Part One: Compute diagnostic code
+	{
+		println!("Part One: {:?}", ());
+	}
+
+	// Part Two: Compute diagnostic code for System ID 5
+	{
+		println!("Part Two: {:?}", ());
+	}
 }
