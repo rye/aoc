a242b67e0bd8bdbe5fb0119a49bb460d5602313d
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-03 21:17:16 -0600

    d2020-03: Rewrite to use new slope helper
    Elegant!
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2020/d2020-03/src/main.rs
+++ 2020/d2020-03/src/main.rs
@@ -37,63 +37,14 @@ fn main() {
 		.collect();
 
 	{
-		let mut x = 0;
-		let mut y = 0;
-		let mut trees = 0;
-
-		let data = data.clone();
-
-		loop {
-			let c: char = data[y][x];
-
-			if c == '#' {
-				trees += 1;
-			}
-
-			if y >= data.len() - 1 {
-				break;
-			} else {
-				x += 3;
-				y += 1;
-				if x >= data[y].len() {
-					x %= data[y].len();
-				}
-			}
-		}
-
-		println!("Part One: {:?}", trees);
+		println!("Part One: {:?}", slope(&data, (3, 1)));
 	}
 
 	{
-		let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-		let data = data.clone();
-		let mut result: usize = 1;
-
-		for slope in slopes {
-			let mut x = 0;
-			let mut y = 0;
-			let mut tree_hits = 0;
-
-			loop {
-				let c: char = data[y][x];
-
-				if c == '#' {
-					tree_hits += 1;
-				}
-
-				if y >= data.len() - 1 {
-					break;
-				} else {
-					x += slope.0;
-					y += slope.1;
-					if x >= data[y].len() {
-						x %= data[y].len();
-					}
-				}
-			}
-
-			result *= tree_hits;
-		}
+		let result = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+			.drain(..)
+			.map(|trajectory| slope(&data, trajectory))
+			.fold(1, |acc, x| acc * x);
 
 		println!("Part Two: {:?}", result);
 	}
