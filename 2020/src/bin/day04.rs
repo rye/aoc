4328ba01a6b51e9d6d2c0a8df2d8e6de92177912
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-06 12:48:37 -0600

    day04: Move library functions into library
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2020/src/bin/day04.rs
+++ 2020/src/bin/day04.rs
@@ -1,89 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::io::{stdin, Read};
 
-fn has_required_fields(passport: &HashMap<&str, &str>) -> bool {
-	matches!(
-		(
-			passport.get("byr"),
-			passport.get("iyr"),
-			passport.get("eyr"),
-			passport.get("hgt"),
-			passport.get("hcl"),
-			passport.get("ecl"),
-			passport.get("pid"),
-			passport.get("cid"),
-		),
-		(
-			Some(_),
-			Some(_),
-			Some(_),
-			Some(_),
-			Some(_),
-			Some(_),
-			Some(_),
-			_
-		)
-	)
-}
-
-fn valid_birth_year(s: &str) -> bool {
-	if let Ok(year) = s.parse::<usize>() {
-		(1920..=2002).contains(&year)
-	} else {
-		false
-	}
-}
-
-fn valid_issue_year(s: &str) -> bool {
-	if let Ok(iyr) = s.parse::<usize>() {
-		(2010..=2020).contains(&iyr)
-	} else {
-		false
-	}
-}
-
-fn valid_expiry_year(s: &str) -> bool {
-	if let Ok(eyr) = s.parse::<usize>() {
-		(2020..=2030).contains(&eyr)
-	} else {
-		false
-	}
-}
-
-fn valid_height(s: &str) -> bool {
-	if let Some(first_letter_offset) = s.chars().position(|c| c.is_alphabetic()) {
-		let maybe_number = &s[0..first_letter_offset];
-		if let Ok(number) = maybe_number.parse::<usize>() {
-			let rest = &s[first_letter_offset..];
-
-			match rest {
-				"cm" => (150..=193).contains(&number),
-				"in" => (59..=76).contains(&number),
-				_ => false,
-			}
-		} else {
-			false
-		}
-	} else {
-		false
-	}
-}
-
-fn valid_hair_color(s: &str) -> bool {
-	if let Some(rest) = s.strip_prefix('#') {
-		let is_digit_and_lowercase = rest.chars().all(|ch| {
-			if ch.is_digit(16) && !ch.is_digit(10) {
-				ch.is_lowercase()
-			} else {
-				ch.is_digit(10)
-			}
-		});
-
-		is_digit_and_lowercase
-	} else {
-		false
-	}
-}
+use d2020::day04::*;
 
 fn main() {
 	let data: String = {
@@ -171,6 +89,3 @@ fn main() {
 		println!("Part Two: {:?}", valid_passports.len());
 	}
 }
-
-#[cfg(test)]
-mod tests {}
