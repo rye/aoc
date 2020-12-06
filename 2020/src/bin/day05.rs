e53cb7af453115ed99e71489536967863f6b6f66
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-06 12:50:17 -0600

    day05: Extract library functions into library
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2020/src/bin/day05.rs
+++ 2020/src/bin/day05.rs
@@ -1,31 +1,7 @@
 use std::collections::BTreeSet;
 use std::io::{stdin, BufRead};
 
-fn binary_hone(instructions: &str, left: char, right: char) -> usize {
-	let mut range: core::ops::Range<usize> = 0..2_usize.pow(instructions.len() as u32);
-
-	for instruction in instructions.chars() {
-		let midpoint = (range.start + range.end) / 2;
-
-		match instruction {
-			c if c == left => range = range.start..midpoint,
-			c if c == right => range = midpoint..range.end,
-			_ => panic!("strange instruction"),
-		}
-	}
-
-	range.start
-}
-
-fn seat_id_from_bsp(bsp: &str) -> usize {
-	let fb: &str = &bsp[0..7];
-	let lr: &str = &bsp[7..10];
-
-	let row: usize = binary_hone(fb, 'F', 'B');
-	let column: usize = binary_hone(lr, 'L', 'R');
-
-	row * 2_usize.pow(lr.len() as u32) + column
-}
+use d2020::day05::*;
 
 fn main() {
 	let stdin = stdin();
@@ -58,6 +34,3 @@ fn main() {
 		println!("Part Two: {:?}", my_seat);
 	}
 }
-
-#[cfg(test)]
-mod tests {}
--- 2020/src/day05/mod.rs
+++ 2020/src/day05/mod.rs
@@ -1,1 +1,28 @@
+pub fn binary_hone(instructions: &str, left: char, right: char) -> usize {
+	let mut range: core::ops::Range<usize> = 0..2_usize.pow(instructions.len() as u32);
 
+	for instruction in instructions.chars() {
+		let midpoint = (range.start + range.end) / 2;
+
+		match instruction {
+			c if c == left => range = range.start..midpoint,
+			c if c == right => range = midpoint..range.end,
+			_ => panic!("strange instruction"),
+		}
+	}
+
+	range.start
+}
+
+pub fn seat_id_from_bsp(bsp: &str) -> usize {
+	let fb: &str = &bsp[0..7];
+	let lr: &str = &bsp[7..10];
+
+	let row: usize = binary_hone(fb, 'F', 'B');
+	let column: usize = binary_hone(lr, 'L', 'R');
+
+	row * 2_usize.pow(lr.len() as u32) + column
+}
+
+#[cfg(test)]
+mod tests;
