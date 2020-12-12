236d25e3cb9069b33b9918f5c585934f6af348d8
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-12 14:14:10 -0600

    day11: Extract Seating logic into its own structure
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2020/src/day11/main.rs
+++ d2020/src/day11/main.rs
@@ -35,35 +35,85 @@ struct StateChange {
 	new_state: CellState,
 }
 
+#[derive(Clone)]
+struct Seating {
+	width: usize,
+	height: usize,
+	seats: Vec<Vec<Cell>>,
+}
+
+impl<'x> core::iter::FromIterator<&'x str> for Seating {
+	fn from_iter<T: IntoIterator<Item = &'x str>>(iter: T) -> Self {
+		let lines: Vec<&str> = iter.into_iter().collect();
+
+		let height = lines.len();
+		let width = lines[0].len();
+
+		let seats: Vec<Vec<Cell>> = lines
+			.iter()
+			.map(|line| line.chars().map(|c| Cell::from(c)).collect())
+			.collect();
+
+		Self {
+			width,
+			height,
+			seats,
+		}
+	}
+}
+
+impl Seating {
+	fn has_seat(&self, x: usize, y: usize) -> bool {
+		(0..self.width).contains(&x) && (0..self.height).contains(&y)
+	}
+
+	fn seat_state(&self, x: usize, y: usize) -> Option<&CellState> {
+		self
+			.seats
+			.get(y)
+			.map(|row| row.get(x).map(|cell| &cell.0))
+			.flatten()
+	}
+
+	fn seat_positions<'x>(&'x self) -> impl Iterator<Item = (usize, usize)> + 'x {
+		(0..self.height)
+			.map(move |y| (0..self.width).map(move |x| (x, y)))
+			.flatten()
+	}
+
+	fn change_seat_state(&mut self, x: usize, y: usize, new_state: CellState) {
+		assert!(self.has_seat(x, y));
+		self.seats[y][x].0 = new_state;
+	}
+
+	fn seats_with_state<'x>(
+		&'x self,
+		state: &'x CellState,
+	) -> impl Iterator<Item = (usize, usize)> + 'x {
+		self
+			.seat_positions()
+			.filter(move |pos| self.seat_state(pos.0, pos.1) == Some(state))
+	}
+}
+
 fn main() {
 	let mut stdin = stdin();
 	let mut data = String::new();
 	stdin.read_to_string(&mut data).unwrap();
 
-	let seats: Vec<Vec<Cell>> = data
-		.lines()
-		.map(|line| line.chars().map(|c| Cell::from(c)).collect())
-		.collect();
-
-	// dbg!(seats);
+	let seats: Seating = data.lines().collect();
 
 	{
 		let mut seats = seats.clone();
 		let mut day = 0;
 
-		let height = seats.len();
-		let width = seats[0].len();
+		let positions: Vec<(usize, usize)> = seats.seat_positions().collect();
 
 		loop {
-			let positions: Vec<(usize, usize)> = (0..seats.len())
-				.map(|y| (0..seats[y].len()).map(move |x| (x, y)))
-				.flatten()
-				.collect();
-
 			// Each empty seat with no adjacent, occupied seats becomes occupied
 			let become_occupied: Vec<StateChange> = positions
 				.iter()
-				.filter(|pos| -> bool { seats[pos.1][pos.0].0 == CellState::Empty })
+				.filter(|pos| -> bool { seats.seat_state(pos.0, pos.1) == Some(&CellState::Empty) })
 				.filter(|pos| -> bool {
 					let top_left: (usize, usize) = (pos.0.saturating_sub(1), pos.1.saturating_sub(1));
 					let bottom_right: (usize, usize) = (pos.0.saturating_add(1), pos.1.saturating_add(1));
@@ -71,24 +121,24 @@ fn main() {
 					let positions_to_check = (top_left.1..=bottom_right.1)
 						.map(|y| (top_left.0..=bottom_right.0).map(move |x| (x, y)))
 						.flatten()
-						.filter(|pos| pos.1 < seats.len() && pos.0 < seats[pos.1].len())
+						.filter(|pos| seats.has_seat(pos.0, pos.1))
 						.filter(|pos_i| &pos_i != pos);
 
 					let occupied_adjacents: Vec<(usize, usize)> = positions_to_check
-						.filter(|pos| seats[pos.1][pos.0].0 == CellState::Occupied)
+						.filter(|pos| seats.seat_state(pos.0, pos.1) == Some(&CellState::Occupied))
 						.collect();
 
 					occupied_adjacents.len() == 0
 				})
 				.map(|pos| -> StateChange {
 					let position: (usize, usize) = *pos;
-					let old_state: CellState = seats[position.1][position.0].0;
-					assert_eq!(old_state, CellState::Empty);
+					let old_state: &CellState = seats.seat_state(pos.0, pos.1).unwrap();
+					assert_eq!(old_state, &CellState::Empty);
 					let new_state: CellState = CellState::Occupied;
 
 					StateChange {
 						position,
-						old_state,
+						old_state: *old_state,
 						new_state,
 					}
 				})
@@ -103,7 +153,7 @@ fn main() {
 			// Each occupied seat with four or more adjacent seats becomes empty
 			let become_emptied: Vec<StateChange> = positions
 				.iter()
-				.filter(|pos| -> bool { seats[pos.1][pos.0].0 == CellState::Occupied })
+				.filter(|pos| -> bool { seats.seat_state(pos.0, pos.1) == Some(&CellState::Occupied) })
 				.filter(|pos| -> bool {
 					let top_left: (usize, usize) = (pos.0.saturating_sub(1), pos.1.saturating_sub(1));
 					let bottom_right: (usize, usize) = (pos.0.saturating_add(1), pos.1.saturating_add(1));
@@ -111,24 +161,24 @@ fn main() {
 					let positions_to_check = (top_left.1..=bottom_right.1)
 						.map(|y| (top_left.0..=bottom_right.0).map(move |x| (x, y)))
 						.flatten()
-						.filter(|pos| pos.1 < seats.len() && pos.0 < seats[pos.1].len())
+						.filter(|pos| seats.has_seat(pos.0, pos.1))
 						.filter(|pos_i| &pos_i != pos);
 
 					let occupied_adjacents: Vec<(usize, usize)> = positions_to_check
-						.filter(|pos| seats[pos.1][pos.0].0 == CellState::Occupied)
+						.filter(|pos| seats.seat_state(pos.0, pos.1) == Some(&CellState::Occupied))
 						.collect();
 
 					occupied_adjacents.len() >= 4
 				})
 				.map(|pos| -> StateChange {
 					let position: (usize, usize) = *pos;
-					let old_state: CellState = seats[position.1][position.0].0;
-					assert_eq!(old_state, CellState::Occupied);
+					let old_state: &CellState = seats.seat_state(pos.0, pos.1).unwrap();
+					assert_eq!(old_state, &CellState::Occupied);
 					let new_state: CellState = CellState::Empty;
 
 					StateChange {
 						position,
-						old_state,
+						old_state: *old_state,
 						new_state,
 					}
 				})
@@ -145,29 +195,19 @@ fn main() {
 			} else {
 				for change in become_occupied {
 					let pos = change.position;
-					assert_eq!(seats[pos.1][pos.0].0, change.old_state);
-					seats[pos.1][pos.0].0 = change.new_state;
+					seats.change_seat_state(pos.0, pos.1, change.new_state);
 				}
 
 				for change in become_emptied {
 					let pos = change.position;
-					assert_eq!(seats[pos.1][pos.0].0, change.old_state);
-					seats[pos.1][pos.0].0 = change.new_state;
+					seats.change_seat_state(pos.0, pos.1, change.new_state);
 				}
 
 				day += 1;
 			}
 		}
 
-		let occupied_count: usize = seats
-			.iter()
-			.map(|row| {
-				row
-					.iter()
-					.filter(|seat| seat.0 == CellState::Occupied)
-					.count()
-			})
-			.sum();
+		let occupied_count: usize = seats.seats_with_state(&CellState::Occupied).count();
 
 		println!("Part One: {:?}", occupied_count);
 	}
