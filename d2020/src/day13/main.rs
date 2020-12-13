7f4bd54f45172aa82b1c0086dddc1cecdf5336da
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-13 15:44:38 -0600

    day13: Add initial step
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ d2020/src/day13/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused_imports)]
+
+use std::io::{stdin, Read};
+use std::{collections::*, str::FromStr};
+
+use regex::Regex;
+
+use d2020::day13::*;
+
+fn main() {
+	let mut stdin = stdin();
+	let mut data = String::new();
+	stdin.read_to_string(&mut data).unwrap();
+
+	{
+		println!("Part One: {:?}", ());
+	}
+
+	{
+		println!("Part Two: {:?}", ());
+	}
+}
--- d2020/src/lib.rs
+++ d2020/src/lib.rs
@@ -10,3 +10,4 @@ pub mod day06;
 pub mod day07;
 pub mod day11;
 pub mod day12;
+pub mod day13;
