0a2b3eb73355074a9a567bb5b5ea6aa31d32d268
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-13 16:26:05 -0600

    Add stubs for days 8 and 9
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ d2020/src/day09/main.rs
@@ -0,0 +1,20 @@
+use std::collections::*;
+use std::io::{stdin, Read};
+
+use regex::Regex;
+
+use d2020::day09::*;
+
+fn main() {
+	let mut stdin = stdin();
+	let mut data = String::new();
+	stdin.read_to_string(&mut data).unwrap();
+
+	{
+		println!("Part One: {:?}", ());
+	}
+
+	{
+		println!("Part Two: {:?}", ());
+	}
+}
