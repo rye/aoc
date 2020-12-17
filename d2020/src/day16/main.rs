908b6cf42c285626fb1dcde411d88a89b2e48a8f
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-16 19:47:10 -0600

    day16: Add stub and inputs
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ d2020/src/day16/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused_imports)]
+
+use std::{
+	collections::*,
+	io::{stdin, Read},
+};
+
+use core::{ops::RangeInclusive, str::FromStr, str::Lines};
+
+use d2020::{day16::*, *};
+
+fn main() {
+	let data: String = string_from(stdin()).unwrap();
+
+	{
+		println!("Part One: {:?}", ());
+	}
+
+	{
+		println!("Part Two: {:?}", ());
+	}
+}
