e28f358495be473c8f66790034d294e12d4f661e
Kristofer Rye <kristofer.rye@gmail.com>
2020-12-16 20:13:54 -0600

    day16: Add a splitter function for breaking input into chunks
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2020/src/day16/main.rs
+++ d2020/src/day16/main.rs
@@ -12,6 +12,8 @@ use d2020::{day16::*, *};
 fn main() {
 	let data: String = string_from(stdin()).unwrap();
 
+	let file_parts = file_parts(data);
+
 	{
 		println!("Part One: {:?}", ());
 	}
@@ -19,6 +21,18 @@ fn main() {
 	{
 		println!("Part Two: {:?}", ());
 	}
+}
+
+/// Break the input into chunks based on the "your ticket:" and "nearby tickets:" separators.
+fn file_parts(data: String) -> Vec<String> {
+	let split_a: Vec<&str> = data.split("\n\nyour ticket:\n").collect();
+	let split_b: Vec<&str> = split_a[1].split(&"\n\nnearby tickets:\n").collect();
+
+	let rules = split_a[0].to_string();
+	let my_ticket = split_b[0].to_string();
+	let nearby_tickets = split_b[1].to_string();
+
+	vec![rules, my_ticket, nearby_tickets]
 }
 
 /// Parse a range which has potentially multiple "parts" into a Vec of RangeInclusive's
@@ -44,7 +58,7 @@ fn parse_multirange(data: &str) -> Vec<RangeInclusive<u32>> {
 
 #[cfg(test)]
 mod tests {
-	use super::parse_multirange;
+	use super::{file_parts, parse_multirange};
 
 	#[cfg(test)]
 	mod parse_multirange {
@@ -66,6 +80,19 @@ mod tests {
 				parse_multirange("1-3 or 4-6 or 77128-192571"),
 				vec![1..=3, 4..=6, 77128..=192571]
 			);
+		}
+	}
+
+	#[cfg(test)]
+	mod file_parts {
+		use super::file_parts;
+
+		#[test]
+		fn proper_line_splits() {
+			let simple_example: String =
+				"a: 1-3\n\nyour ticket:\n1\n\nnearby tickets:\n1\n2\n3".to_string();
+
+			assert_eq!(file_parts(simple_example), vec!["a: 1-3", "1", "1\n2\n3"]);
 		}
 	}
 }
