0c914b7064eff6aa8a11ded506d9edf6debea5d0
Kristofer Rye <kristofer.rye@gmail.com>
2021-01-17 15:46:02 -0600

    day06: Refactor to use new day solver macro
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2020/src/day06/mod.rs
+++ d2020/src/day06/mod.rs
@@ -23,5 +23,36 @@ pub fn intersect_all(items: impl Iterator<Item = BTreeSet<Answer>>) -> Option<BT
 	)
 }
 
+pub type Intermediate<'a> = Vec<&'a str>;
+pub type Solution = usize;
+
+pub fn parse(data: &str) -> Intermediate {
+	data.split("\n\n").collect()
+}
+
+pub fn part_one(groups: &Intermediate) -> Option<Solution> {
+	Some(
+		groups
+			.iter()
+			.map(|group| answers(group).collect())
+			.map(|answers: BTreeSet<Answer>| answers.len())
+			.sum(),
+	)
+}
+
+pub fn part_two(groups: &Intermediate) -> Option<Solution> {
+	Some(
+		groups
+			.iter()
+			.map(|group| {
+				let answers_by_person = people_in_group(group).map(|person| answers(person).collect());
+				let answers_by_all = intersect_all(answers_by_person);
+
+				answers_by_all.expect("no people in group").len()
+			})
+			.sum(),
+	)
+}
+
 #[cfg(test)]
 mod tests;
