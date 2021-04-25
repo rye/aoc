b73085136134bdafeda5a33a303e6c0ed12d3be3
Kristofer Rye <kristofer.rye@gmail.com>
2021-04-25 14:25:44 -0500

    day18: Finish part one, rework, add more tests
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2020/src/day18/mod.rs
+++ d2020/src/day18/mod.rs
@@ -1,14 +1,39 @@
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Token {
-	Number(i64),
+	Number(u64),
 	OpenParen,
 	CloseParen,
 	Asterisk,
 	Plus,
 }
 
+impl std::fmt::Display for Token {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		match self {
+			&Token::Number(n) => write!(f, "{}", n),
+			&Token::Plus => write!(f, "+"),
+			&Token::Asterisk => write!(f, "*"),
+			&Token::OpenParen => write!(f, "("),
+			&Token::CloseParen => write!(f, ")"),
+		}
+	}
+}
+
 #[derive(Debug)]
 pub struct Expr(Vec<Token>);
+
+impl std::fmt::Display for Expr {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		let disp = self
+			.0
+			.iter()
+			.map(|tok| tok.to_string())
+			.collect::<Vec<String>>()
+			.join(" ");
+
+		write!(f, "{}", disp)
+	}
+}
 
 type Intermediate = Vec<Expr>;
 type Solution = u64;
@@ -31,7 +56,7 @@ pub fn parse(input: &str) -> Intermediate {
 				}
 			} else {
 				if let Some(n) = number {
-					tokens.push(Token::Number(n as i64));
+					tokens.push(Token::Number(n as u64));
 					number = None;
 				}
 
@@ -48,10 +73,8 @@ pub fn parse(input: &str) -> Intermediate {
 		}
 
 		if let Some(n) = number {
-			tokens.push(Token::Number(n as i64));
+			tokens.push(Token::Number(n as u64));
 		}
-
-		println!("Line: {} => {:?}", line, tokens);
 
 		exprs.push(Expr(tokens));
 	}
@@ -59,252 +82,437 @@ pub fn parse(input: &str) -> Intermediate {
 	exprs
 }
 
-fn check_parenbal(expr: &Expr) -> bool {
-	let mut level: usize = 0;
-	let mut balanced: bool = true;
+impl Expr {
+	fn is_paren_balanced(&self) -> bool {
+		let mut level: usize = 0;
+		let mut balanced: bool = true;
 
-	for token in expr.0.iter() {
-		match token {
-			Token::OpenParen => {
-				level += 1;
-			}
-			Token::CloseParen => {
-				if level > 0 {
-					level -= 1;
-				} else {
-					balanced = false;
-					break;
+		for token in self.0.iter() {
+			match token {
+				Token::OpenParen => {
+					level += 1;
 				}
-			}
-			_ => {}
-		}
-	}
-
-	if level > 0 {
-		balanced = false;
-	}
-
-	balanced
-}
-
-pub fn eval_expr(expr: &Expr) -> Option<i64> {
-	if !check_parenbal(expr) {
-		return None;
-	}
-
-	let mut result: Option<i64> = None;
-	let mut current_op: Option<Token> = None;
-
-	println!("Evaluating {:?}", expr);
-
-	for (idx, token) in expr.0.iter().enumerate() {
-		println!(
-			"idx={}, token={:?}, result={:?}, current_op={:?}",
-			idx, token, result, current_op
-		);
-
-		match token {
-			Token::Number(n) => {
-				if result == None {
-					result = Some(*n);
-				} else if let Some(Token::Plus) = current_op {
-					result = Some(result.unwrap() + n);
-					current_op = None;
-				} else if let Some(Token::Asterisk) = current_op {
-					result = Some(result.unwrap() * n);
-					current_op = None;
-				} else {
-					panic!()
-				}
-			}
-			Token::Plus => {
-				if current_op.is_none() {
-					current_op = Some(Token::Plus)
-				} else {
-					panic!()
-				}
-			}
-			Token::Asterisk => {
-				if current_op.is_none() {
-					current_op = Some(Token::Asterisk)
-				} else {
-					panic!()
-				}
-			}
-			Token::OpenParen => {
-				let mut matching_close_idx: usize = idx;
-
-				let mut level: usize = 0;
-
-				for close_idx in idx..expr.0.len() {
-					match expr.0[close_idx] {
-						Token::OpenParen => level += 1,
-						Token::CloseParen => {
-							if level > 0 {
-								level -= 1
-							} else {
-								matching_close_idx = close_idx;
-								break;
-							}
-						}
-						_ => {}
+				Token::CloseParen => {
+					if level > 0 {
+						level -= 1;
+					} else {
+						balanced = false;
+						break;
 					}
 				}
-
-				let expr = Expr(expr.0[idx..matching_close_idx].iter().cloned().collect());
-
-				let result = eval_expr(&expr);
-
-				match current_op { _ => todo!() }
+				_ => {}
 			}
-			Token::CloseParen => {},
-			_ => todo!(),
+		}
+
+		if level > 0 {
+			balanced = false;
+		}
+
+		balanced
+	}
+
+	fn is_valid(&self) -> bool {
+		self.is_paren_balanced() && true
+	}
+
+	fn matching_close_idx(span: &[Token], open_idx: usize) -> Option<usize> {
+		assert!(span[open_idx] == Token::OpenParen);
+
+		let mut level = 0;
+
+		// TODO enumerate/scan/find?
+
+		for idx in open_idx..span.len() {
+			match span[idx] {
+				Token::OpenParen => level += 1,
+				Token::CloseParen => {
+					level -= 1;
+					if level == 0 {
+						return Some(idx);
+					}
+				}
+				_ => {}
+			}
+		}
+
+		None
+	}
+
+	/// Evaluates a `(` `)`-encapsulated span consisting of solely +, *, and literlas.
+	fn eval_span(span: &[Token]) -> Option<u64> {
+		let mut value: Option<u64> = None;
+		let mut op: Option<Token> = None;
+
+		for tok in span {
+			match (value, op, tok) {
+				(None, _, Token::Number(n)) => value = Some(*n),
+				(None, _, _) => panic!("need a starting value first"),
+				(Some(_), None, Token::Plus) => op = Some(Token::Plus),
+				(Some(_), None, Token::Asterisk) => op = Some(Token::Asterisk),
+
+				(Some(cur), Some(Token::Plus), Token::Number(n)) => {
+					value = Some(cur + n);
+					op = None;
+				}
+				(Some(cur), Some(Token::Asterisk), Token::Number(n)) => {
+					value = Some(cur * n);
+					op = None;
+				}
+				(Some(_), _, _) => panic!(
+					"unexpected span layout, cur={:?}, op={:?}, tok={:?}",
+					value, op, tok
+				),
+			}
+		}
+
+		value
+	}
+
+	fn evaluate(&self) -> Option<u64> {
+		if !self.is_valid() {
+			return None;
+		}
+
+		// Flatten out the expression, evaluating subexpressions as they are found.
+		if let Some(open_paren_idx) = self
+			.0
+			.iter()
+			.enumerate()
+			.find(|(_, tok)| **tok == Token::OpenParen)
+			.map(|res| res.0)
+		{
+			let matching_close_paren_idx = Self::matching_close_idx(&self.0, open_paren_idx).unwrap();
+
+			let span_to_evaluate = (open_paren_idx + 1)..matching_close_paren_idx;
+
+			let subexpr = Expr(self.0[span_to_evaluate.clone()].to_vec());
+
+			let result = subexpr
+				.evaluate()
+				.expect("inner expr failed to evaluate to a value");
+
+			let mut new_expr_body = self.0.clone();
+			let _ = new_expr_body.splice(
+				open_paren_idx..=matching_close_paren_idx,
+				vec![Token::Number(result)],
+			);
+
+			let new_expr = Expr(new_expr_body);
+
+			new_expr.evaluate()
+		} else {
+			Self::eval_span(&self.0)
+		}
+	}
+}
+
+#[cfg(test)]
+mod expr {
+
+	use super::{Expr, Token, Token::*};
+
+	#[cfg(test)]
+	mod is_paren_balanced {
+
+		use super::*;
+
+		#[test]
+		fn o() {
+			assert_eq!(Expr(vec![OpenParen,]).is_paren_balanced(), false);
+		}
+
+		#[test]
+		fn c() {
+			assert_eq!(Expr(vec![CloseParen,]).is_paren_balanced(), false);
+		}
+
+		#[test]
+		fn oc() {
+			assert_eq!(Expr(vec![OpenParen, CloseParen,]).is_paren_balanced(), true);
+		}
+
+		#[test]
+		fn co() {
+			assert_eq!(
+				Expr(vec![CloseParen, OpenParen,]).is_paren_balanced(),
+				false
+			);
+		}
+
+		#[test]
+		fn oocc() {
+			assert_eq!(
+				Expr(vec![OpenParen, OpenParen, CloseParen, CloseParen,]).is_paren_balanced(),
+				true
+			);
+		}
+
+		#[test]
+		fn ococ() {
+			assert_eq!(
+				Expr(vec![OpenParen, CloseParen, OpenParen, CloseParen,]).is_paren_balanced(),
+				true
+			);
 		}
 	}
 
-	result
-}
+	#[cfg(test)]
+	mod eval_span {
+		use super::*;
 
-#[test]
-fn parenbal_o() {
-	assert_eq!(check_parenbal(&Expr(vec![Token::OpenParen,])), false);
-}
+		#[test]
+		fn s1p2m3p4m5p6() {
+			assert_eq!(
+				Expr::eval_span(&vec![
+					Number(1),
+					Plus,
+					Number(2),
+					Asterisk,
+					Number(3),
+					Plus,
+					Number(4),
+					Asterisk,
+					Number(5),
+					Plus,
+					Number(6),
+				]),
+				Some(71)
+			);
+		}
+	}
 
-#[test]
-fn parenbal_c() {
-	assert_eq!(check_parenbal(&Expr(vec![Token::CloseParen,])), false);
-}
+	#[cfg(test)]
+	mod evaluate {
+		use super::*;
 
-#[test]
-fn parenbal_oc() {
-	assert_eq!(
-		check_parenbal(&Expr(vec![Token::OpenParen, Token::CloseParen,])),
-		true
-	);
-}
+		#[test]
+		fn e71() {
+			assert_eq!(
+				Expr(vec![
+					Number(1),
+					Plus,
+					Number(2),
+					Asterisk,
+					Number(3),
+					Plus,
+					Number(4),
+					Asterisk,
+					Number(5),
+					Plus,
+					Number(6),
+				])
+				.evaluate(),
+				Some(71)
+			)
+		}
 
-#[test]
-fn parenbal_co() {
-	assert_eq!(
-		check_parenbal(&Expr(vec![Token::CloseParen, Token::OpenParen,])),
-		false
-	);
-}
+		#[test]
+		fn e51() {
+			assert_eq!(
+				Expr(vec![
+					Number(1),
+					Plus,
+					OpenParen,
+					Number(2),
+					Asterisk,
+					Number(3),
+					CloseParen,
+					Plus,
+					OpenParen,
+					Number(4),
+					Asterisk,
+					OpenParen,
+					Number(5),
+					Plus,
+					Number(6),
+					CloseParen,
+					CloseParen,
+				])
+				.evaluate(),
+				Some(51)
+			)
+		}
 
-#[test]
-fn parenbal_oocc() {
-	assert_eq!(
-		check_parenbal(&Expr(vec![
-			Token::OpenParen,
-			Token::OpenParen,
-			Token::CloseParen,
-			Token::CloseParen,
-		])),
-		true
-	);
-}
+		#[test]
+		fn e26() {
+			assert_eq!(
+				Expr(vec![
+					Number(2),
+					Asterisk,
+					Number(3),
+					Plus,
+					OpenParen,
+					Number(4),
+					Asterisk,
+					Number(5),
+					CloseParen
+				])
+				.evaluate(),
+				Some(26)
+			)
+		}
 
-#[test]
-fn parenbal_ococ() {
-	assert_eq!(
-		check_parenbal(&Expr(vec![
-			Token::OpenParen,
-			Token::CloseParen,
-			Token::OpenParen,
-			Token::CloseParen,
-		])),
-		true
-	);
-}
+		#[test]
+		fn e437() {
+			assert_eq!(
+				Expr(vec![
+					Number(5),
+					Plus,
+					OpenParen,
+					Number(8),
+					Asterisk,
+					Number(3),
+					Plus,
+					Number(9),
+					Plus,
+					Number(3),
+					Asterisk,
+					Number(4),
+					Asterisk,
+					Number(3),
+					CloseParen
+				])
+				.evaluate(),
+				Some(437)
+			)
+		}
 
-#[test]
-fn eval_expr_71() {
-	use Token::*;
+		#[test]
+		fn e12240() {
+			assert_eq!(
+				Expr(vec![
+					Number(5),
+					Token::Asterisk,
+					Number(9),
+					Token::Asterisk,
+					Token::OpenParen,
+					Number(7),
+					Token::Asterisk,
+					Number(3),
+					Token::Asterisk,
+					Number(3),
+					Token::Plus,
+					Number(9),
+					Token::Asterisk,
+					Number(3),
+					Token::Plus,
+					Token::OpenParen,
+					Number(8),
+					Token::Plus,
+					Number(6),
+					Token::Asterisk,
+					Number(4),
+					CloseParen,
+					CloseParen
+				])
+				.evaluate(),
+				Some(12240)
+			)
+		}
 
-	assert_eq!(
-		eval_expr(&Expr(vec![
-			Number(1),
-			Plus,
-			Number(2),
-			Asterisk,
-			Number(3),
-			Plus,
-			Number(4),
-			Asterisk,
-			Number(5),
-			Plus,
-			Number(6),
-		])),
-		Some(71)
-	)
-}
+		#[test]
+		fn e13632() {
+			assert_eq!(
+				Expr(vec![
+					OpenParen,
+					OpenParen,
+					Number(2),
+					Plus,
+					Number(4),
+					Asterisk,
+					Number(9),
+					CloseParen,
+					Asterisk,
+					OpenParen,
+					Number(6),
+					Plus,
+					Number(9),
+					Asterisk,
+					Number(8),
+					Plus,
+					Number(6),
+					CloseParen,
+					Plus,
+					Number(6),
+					CloseParen,
+					Plus,
+					Number(2),
+					Plus,
+					Number(4),
+					Asterisk,
+					Number(2)
+				])
+				.evaluate(),
+				Some(13632)
+			)
+		}
+	}
 
-#[test]
-fn eval_expr_51() {
-	use Token::*;
+	#[cfg(test)]
+	mod matching_close_idx {
+		use super::*;
 
-	assert_eq!(
-		eval_expr(&Expr(vec![
-			Number(1),
-			Plus,
-			OpenParen,
-			Number(2),
-			Asterisk,
-			Number(3),
-			CloseParen,
-			Plus,
-			OpenParen,
-			Number(4),
-			Asterisk,
-			OpenParen,
-			Number(5),
-			Plus,
-			Number(6),
-			CloseParen,
-			CloseParen,
-		])),
-		Some(51)
-	)
-}
+		#[test]
+		fn onpnc() {
+			// ( 1 + 2 )
+			let span = vec![OpenParen, Number(1), Plus, Number(2), CloseParen];
 
-#[test]
-fn eval_expr_26() {
-	use Token::*;
-	assert_eq!(
-		eval_expr(&Expr(vec![
-			Number(2),
-			Asterisk,
-			Number(3),
-			Plus,
-			OpenParen,
-			Number(4),
-			Asterisk,
-			Number(5),
-			CloseParen
-		])),
-		Some(26)
-	)
-}
+			assert_eq!(Expr::matching_close_idx(&span, 0), Some(4));
+		}
 
-#[test]
-fn eval_expr_437() {
-	use Token::*;
-	assert_eq!(
-		eval_expr(&Expr(vec![
-			Number(5),
-			Plus,
-			OpenParen,
-			Number(8),
-			Asterisk,
-			Number(3),
-			CloseParen
-		])),
-		Some(437)
-	)
+		#[test]
+		fn onpnponmncc() {
+			// ( 1 + 2 + ( 3 * 4 ) )
+			let span = vec![
+				OpenParen,
+				Number(1),
+				Plus,
+				Number(2),
+				Plus,
+				OpenParen,
+				Number(3),
+				Asterisk,
+				Number(4),
+				CloseParen,
+				CloseParen,
+			];
+
+			assert_eq!(Expr::matching_close_idx(&span, 0), Some(10));
+			assert_eq!(Expr::matching_close_idx(&span, 5), Some(9));
+		}
+
+		#[test]
+		fn onpnponmncponpncc() {
+			// ( 1 + 2 + ( 3 * 4 ) + ( 5 + 6 ) )
+			let span = vec![
+				OpenParen,
+				Number(1),
+				Plus,
+				Number(2),
+				Plus,
+				OpenParen,
+				Number(3),
+				Asterisk,
+				Number(4),
+				CloseParen,
+				Plus,
+				OpenParen,
+				Number(5),
+				Plus,
+				Number(6),
+				CloseParen,
+				CloseParen,
+			];
+
+			assert_eq!(Expr::matching_close_idx(&span, 0), Some(16));
+			assert_eq!(Expr::matching_close_idx(&span, 5), Some(9));
+			assert_eq!(Expr::matching_close_idx(&span, 11), Some(15));
+		}
+	}
 }
 
 pub fn part_one(exprs: &Intermediate) -> Option<Solution> {
-	None
+	exprs.iter().map(|expr| expr.evaluate()).sum()
 }
 
 pub fn part_two(exprs: &Intermediate) -> Option<Solution> {
