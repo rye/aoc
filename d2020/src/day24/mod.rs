072c21c4d460a467a3833a9bad295dd8e35923ca
Kristofer Rye <kristofer.rye@gmail.com>
2021-07-29 09:44:12 -0500

    day2{3,4}: Set up files with nothing happening yet
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ d2020/src/day24/mod.rs
@@ -0,0 +1,10 @@
+type Intermediate = ();
+type Solution = usize;
+
+pub fn parse(_: &str) {}
+pub fn part_one(_: &Intermediate) -> Option<Solution> {
+	None
+}
+pub fn part_two(_: &Intermediate) -> Option<Solution> {
+	None
+}
