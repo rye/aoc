ca0b9e013f71573beff2027ca58e47b76abc7c58
Kristofer Rye <kristofer.rye@gmail.com>
2021-10-23 19:18:59 -0500

    2015/04: Implement part one and part two
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2015/src/day04/mod.rs
+++ d2015/src/day04/mod.rs
@@ -1,13 +1,39 @@
-type Intermediate = ();
+type Intermediate = String;
 
-pub fn parse(_input: &str) -> Intermediate {}
-
-type Solution = ();
-
-pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+pub fn parse(input: &str) -> Intermediate {
+	input.lines().map(str::to_string).collect()
 }
 
-pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+const fn is_zero(byte: u8) -> bool {
+	byte == 0_u8
+}
+
+const fn is_upper_nibble_zero(byte: u8) -> bool {
+	(byte >> 4) == 0_u8
+}
+
+const fn leading5(data: [u8; 16]) -> bool {
+	is_zero(data[0]) && is_zero(data[1]) && is_upper_nibble_zero(data[2])
+}
+
+const fn leading6(data: [u8; 16]) -> bool {
+	is_zero(data[0]) && is_zero(data[1]) && is_zero(data[2])
+}
+
+type Solution = usize;
+
+const RANGE: core::ops::Range<usize> = 1_usize..10000000_usize;
+
+pub fn part_one(stub: &Intermediate) -> Option<Solution> {
+	RANGE
+		.map(|n| (n, md5::compute(format!("{}{}", stub, n))))
+		.find(|n| leading5((n.1).0))
+		.map(|tuple| tuple.0)
+}
+
+pub fn part_two(stub: &Intermediate) -> Option<Solution> {
+	RANGE
+		.map(|n| (n, md5::compute(format!("{}{}", stub, n))))
+		.find(|n| leading6((n.1).0))
+		.map(|tuple| tuple.0)
 }
