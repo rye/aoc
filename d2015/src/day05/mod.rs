3d57a7079365f986cd914d9475e4e507c2b08ce0
Kristofer Rye <kristofer.rye@gmail.com>
2021-10-23 20:34:43 -0500

    2015/05: Implement part two
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2015/src/day05/mod.rs
+++ d2015/src/day05/mod.rs
@@ -51,6 +51,76 @@ pub fn part_one(strings: &Intermediate) -> Option<Solution> {
 	Some(strings.iter().filter(is_nice_old).count())
 }
 
-pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+fn contains_nonoverlapping_pair(string: &str) -> bool {
+	let chars: Vec<char> = string.chars().collect();
+	for window in chars.windows(2) {
+		let pat = format!("{}{}", window[0], window[1]);
+		if string.split(&pat).count() >= 3 {
+			return true;
+		}
+	}
+
+	false
+}
+
+#[test]
+fn aaa_cnp() {
+	assert_eq!(contains_nonoverlapping_pair("aaa"), false);
+}
+
+#[test]
+fn aaaa_cnp() {
+	assert_eq!(contains_nonoverlapping_pair("aaaa"), true);
+}
+
+#[test]
+fn xy0xy_cnp() {
+	assert_eq!(contains_nonoverlapping_pair("xy0xy"), true);
+}
+
+fn contains_one_wrapped_letter(string: &str) -> bool {
+	let chars: Vec<char> = string.chars().collect();
+
+	for window in chars.windows(3) {
+		if window.get(0) == window.get(2) {
+			return true;
+		}
+	}
+
+	false
+}
+
+fn is_nice_new(string: &impl AsRef<str>) -> bool {
+	let string: &str = string.as_ref();
+
+	contains_nonoverlapping_pair(string) && contains_one_wrapped_letter(string)
+}
+
+pub fn part_two(strings: &Intermediate) -> Option<Solution> {
+	Some(strings.iter().filter(is_nice_new).count())
+}
+
+#[cfg(test)]
+mod is_nice_new {
+	use super::is_nice_new;
+
+	#[test]
+	fn qjhvhtzxzqqjkmpb_is_nice() {
+		assert_eq!(is_nice_new(&"qjhvhtzxzqqjkmpb"), true);
+	}
+
+	#[test]
+	fn xxyxx_is_nice() {
+		assert_eq!(is_nice_new(&"xxyxx"), true);
+	}
+
+	#[test]
+	fn uurcxstgmygtbstg_is_naughty() {
+		assert_eq!(is_nice_new(&"uurcxstgmygtbstg"), false);
+	}
+
+	#[test]
+	fn ieodomkazucvgmuy_is_nice() {
+		assert_eq!(is_nice_new(&"ieodomkazucvgmuy"), false);
+	}
 }
