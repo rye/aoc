0a9d3996c294d74b4c6f1e5a1531fc9650b3bf36
Kristofer Rye <kristofer.rye@gmail.com>
2021-10-24 13:47:40 -0500

    2015/06: Implement instruction parsing with error handling
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2015/src/day06/mod.rs
+++ d2015/src/day06/mod.rs
@@ -1,6 +1,148 @@
-type Intermediate = ();
+#[derive(Debug, PartialEq)]
+enum Action {
+	TurnOn,
+	TurnOff,
+	Toggle,
+}
 
-pub fn parse(_input: &str) -> Intermediate {}
+#[derive(Debug, PartialEq)]
+struct Coord([usize; 2]);
+
+impl core::str::FromStr for Coord {
+	type Err = Error;
+
+	fn from_str(string: &str) -> Result<Self, Self::Err> {
+		let coords: Vec<&str> = string.split(',').collect();
+
+		if coords.len() != 2 {
+			Err(Error::CoordParse)
+		} else {
+			let w = coords[0].parse::<usize>().ok();
+			let h = coords[1].parse::<usize>().ok();
+
+			if let (Some(w), Some(h)) = (w, h) {
+				Ok(Coord([w, h]))
+			} else {
+				Err(Error::CoordParse)
+			}
+		}
+	}
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+	action: Action,
+	start: Coord,
+	end: Coord,
+}
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum Error {
+	#[error("required keyword 'through' not found in instruction range specifier")]
+	ThroughNotFound,
+	#[error("Coord could not be parsed")]
+	CoordParse,
+	#[error("missing a required part of an instruction")]
+	MissingPart,
+	#[error("unrecognized token")]
+	UnrecognizedToken,
+}
+
+impl core::str::FromStr for Instruction {
+	type Err = Error;
+
+	fn from_str(string: &str) -> Result<Self, Self::Err> {
+		let chunks: Vec<&str> = string.split(' ').collect();
+
+		let action: Action = match chunks.get(0) {
+			Some(&"toggle") => Ok(Action::Toggle),
+			Some(&"turn") => match chunks.get(1) {
+				Some(&"on") => Ok(Action::TurnOn),
+				Some(&"off") => Ok(Action::TurnOff),
+				_ => Err(Error::UnrecognizedToken),
+			},
+			_ => Err(Error::UnrecognizedToken),
+		}?;
+
+		let through_index: usize = chunks
+			.iter()
+			.position(|&chunk| chunk == "through")
+			.ok_or(Error::ThroughNotFound)?;
+
+		let start: Coord = chunks
+			.get(through_index - 1)
+			.ok_or(Error::MissingPart)?
+			.parse::<Coord>()?;
+
+		let end: Coord = chunks
+			.get(through_index + 1)
+			.ok_or(Error::MissingPart)?
+			.parse::<Coord>()?;
+
+		Ok(Instruction { action, start, end })
+	}
+}
+
+#[cfg(test)]
+mod instruction_fromstr {
+	use super::{Action, Coord, Instruction};
+	#[test]
+	fn turn_on_0_0_through_999_999() {
+		assert_eq!(
+			"turn on 0,0 through 999,999".parse::<Instruction>(),
+			Ok(Instruction {
+				action: Action::TurnOn,
+				start: Coord([0, 0]),
+				end: Coord([999, 999]),
+			})
+		)
+	}
+
+	#[test]
+	fn toggle_0_0_through_999_0() {
+		assert_eq!(
+			"toggle 0,0 through 999,0".parse::<Instruction>(),
+			Ok(Instruction {
+				action: Action::Toggle,
+				start: Coord([0, 0]),
+				end: Coord([999, 0]),
+			})
+		)
+	}
+
+	#[test]
+	fn turn_off_499_499_through_500_500() {
+		assert_eq!(
+			"turn off 499,499 through 500,500".parse::<Instruction>(),
+			Ok(Instruction {
+				action: Action::TurnOff,
+				start: Coord([499, 499]),
+				end: Coord([500, 500]),
+			})
+		)
+	}
+}
+
+enum LightState {
+	Off,
+	On,
+}
+
+struct Light(LightState);
+
+struct Grid {
+	lights: [[Light; 1000]; 1000],
+}
+
+type Intermediate = Vec<Instruction>;
+
+pub fn parse(input: &str) -> Intermediate {
+	input
+		.lines()
+		.map(core::str::FromStr::from_str)
+		.filter_map(Result::ok)
+		.collect()
+}
 
 type Solution = usize;
 
