376a7c46cf3f5e157b0abb5fff11d56b8a48c129
Kristofer Rye <kristofer.rye@gmail.com>
2021-11-07 15:29:57 -0600

    2015/11: Initialize
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ d2015/src/day11/mod.rs
@@ -0,0 +1,13 @@
+type Intermediate = ();
+
+pub fn parse(_input: &str) -> Intermediate {}
+
+type Solution = String;
+
+pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
+	None
+}
+
+pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
+	None
+}
--- d2015/src/lib.rs
+++ d2015/src/lib.rs
@@ -8,6 +8,7 @@ pub mod day07;
 // day 08 is implemented in Python; see src/day08.py
 pub mod day09;
 pub mod day10;
+pub mod day11;
 
 /// Fully consumes a reader of type `std::io::Read` and produces a `String` containing all read text.
 ///
