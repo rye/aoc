1f237c2fba6c63aa295e8b2c61addca4303abe9d
Kristofer Rye <kristofer.rye@gmail.com>
2021-11-27 12:28:08 -0600

    2021: Prepare
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ d2021/scripts/new-day.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+workdir="$(dirname $0)/.."
+year="2021"
+
+daynumber="$1"
+daynumber2d="$(printf '%02d' $daynumber)"
+
+if [ -d "${workdir}/src/day${daynumber2d}" ];
+then
+	>&2 echo "Directory src/day${daynumber2d} already exists. Not creating again."
+else
+	mkdir -pv "${workdir}/src/day${daynumber2d}"
+fi
+
+if [ -f "${workdir}/src/day${daynumber2d}/mod.rs" ];
+then
+	>&2 echo "Module src/day${daynumber2d}/mod.rs already exists. Not creating again."
+else
+	cat > "${workdir}/src/day${daynumber2d}/mod.rs" << EOM
+type Intermediate = ();
+
+pub fn parse(_input: &str) -> Intermediate {}
+
+type Solution = ();
+
+pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
+	None
+}
+
+pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
+	None
+}
+EOM
+fi
+
+if [ -f "${workdir}/src/bin/day${daynumber2d}.rs" ];
+then
+	>&2 echo "Binary src/bin/day${daynumber2d}.rs already exists. Not creating again."
+else
+	cat > "${workdir}/src/bin/day${daynumber2d}.rs" << EOM
+d${year}::day_solver_from!(d${year}::day${daynumber2d});
+EOM
+fi
---
+++ d2021/src/lib.rs
@@ -0,0 +1,104 @@
+pub mod day01;
+
+/// Fully consumes a reader of type `std::io::Read` and produces a `String` containing all read text.
+///
+/// # Errors
+///
+/// An error is only returned if the underlying [`std::io::Read::read_to_string`] operation
+/// returns an error.
+///
+/// See [`std::io::Read::read_to_string`] for all error semantics.
+pub fn string_from(mut read: impl std::io::Read) -> std::io::Result<String> {
+	let mut buf: String = String::new();
+	read.read_to_string(&mut buf)?;
+	Ok(buf)
+}
+
+/// Produces a "day solver" `main` definition.
+///
+/// # Generated Code
+///
+/// Arguments `$transform`, `$part_one`, and `$part_two` are all treated as expressions to be
+/// evaluated against the underlying input after it has been processed.
+///
+#[macro_export]
+macro_rules! day_solver {
+	( $transform:expr, $part_one:expr, $part_two:expr ) => {
+		fn main() -> Result<(), Box<dyn std::error::Error>> {
+			use std::io::stdin;
+			use $crate::string_from;
+
+			let data: String = string_from(stdin())?;
+
+			let intermediate = $transform(&data);
+
+			if let Some(part_one) = $part_one(&intermediate) {
+				println!("Part One: {}", part_one);
+			}
+
+			if let Some(part_two) = $part_two(&intermediate) {
+				println!("Part Two: {}", part_two);
+			}
+
+			Ok(())
+		}
+	};
+}
+
+/// Generates a "day" solver from a given module.
+///
+/// # Examples
+///
+/// ```rust
+/// mod dayXX {
+/// 	type Intermediate = usize;
+///
+/// 	pub(super) fn parse(input: &str) -> Intermediate {
+/// 		input.lines().count()
+/// 	}
+///
+/// 	type Solution = usize;
+///
+/// 	pub(super) fn part_one(line_count: &Intermediate) -> Option<Solution> {
+/// 		Some(*line_count)
+/// 	}
+///
+/// 	pub(super) fn part_two(line_count: &Intermediate) -> Option<Solution> {
+/// 		Some(*line_count)
+/// 	}
+/// }
+///
+/// d2021::day_solver_from!(dayXX);
+/// ```
+#[macro_export]
+macro_rules! day_solver_from {
+	($place:path) => {
+		use $place::{parse, part_one, part_two};
+
+		d2021::day_solver!(
+			|data| { parse(data) },
+			|intermediate| { part_one(intermediate) },
+			|intermediate| { part_two(intermediate) }
+		);
+	};
+
+	($place:path, $parser:ident) => {
+		use $place::{part_one, part_two, $parser};
+
+		d2021::day_solver!(
+			|data| { $parser(data) },
+			|intermediate| { part_one(intermediate) },
+			|intermediate| { part_two(intermediate) }
+		);
+	};
+
+	($place:path, $parser:ident, $part_one:ident, $part_two:ident) => {
+		use $place::{$parser, $part_one, $part_two};
+
+		d2021::day_solver!(
+			|data| { $parser(data) },
+			|intermediate| { $part_one(intermediate) },
+			|intermediate| { $part_two(intermediate) }
+		);
+	};
+}
