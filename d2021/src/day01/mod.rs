c368ed98e9d0bff90a829906b94c168ea887d294
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-01 18:36:38 -0600

    2021/01: Implement part one
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day01/mod.rs
+++ d2021/src/day01/mod.rs
@@ -1,11 +1,24 @@
-type Intermediate = ();
+type Intermediate = Vec<u16>;
 
-pub fn parse(_input: &str) -> Intermediate {}
+pub fn parse(input: &str) -> Intermediate {
+	input
+		.lines()
+		.map(str::parse)
+		.filter_map(Result::ok)
+		.collect()
+}
 
 type Solution = usize;
 
-pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+pub fn part_one(readings: &Intermediate) -> Option<Solution> {
+	Some(
+		readings
+			.windows(2)
+			.map(<&[u16; 2]>::try_from)
+			.filter_map(Result::ok)
+			.filter(|[a, b]| a < b)
+			.count(),
+	)
 }
 
 pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
