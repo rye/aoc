9202eb29d735be1e3305c83e080947a683c012ca
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-03 05:48:46 -0600

    2021/03: Start on parsing code
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ d2021/src/day03/mod.rs
@@ -0,0 +1,15 @@
+type Intermediate<'a> = Vec<&'a str>;
+
+pub fn parse(input: &str) -> Intermediate {
+	input.lines().collect()
+}
+
+type Solution = usize;
+
+pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
+	None
+}
+
+pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
+	None
+}
