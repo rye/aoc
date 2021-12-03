a15a04417ed6cbf631f66035292e6b598acc091b
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-03 06:17:06 -0600

    2021/03: Implement part one
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day03/mod.rs
+++ d2021/src/day03/mod.rs
@@ -1,13 +1,54 @@
-type Intermediate<'a> = Vec<&'a str>;
+type Intermediate = Vec<[char; 12]>;
 
 pub fn parse(input: &str) -> Intermediate {
-	input.lines().collect()
+	input
+		.lines()
+		.map(str::chars)
+		.map(std::str::Chars::collect::<Vec<_>>)
+		.map(<[char; 12]>::try_from)
+		.map(Result::unwrap)
+		.collect()
 }
 
-type Solution = usize;
+// Each string in the input is 12 bits long. This means that they fit in a u16.  However,
+// we need to be able to multiply without overflow.  (log(2^16 * 2^16) / log(2) == 32).
+type Solution = u32;
 
-pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+fn bit_count(accumulator: [[usize; 2]; 12], string: &[char; 12]) -> [[usize; 2]; 12] {
+	let mut accumulator = accumulator;
+
+	for idx in 0..12 {
+		match string[idx] {
+			'0' => accumulator[idx][0] += 1,
+			'1' => accumulator[idx][1] += 1,
+			_ => unreachable!(),
+		}
+	}
+
+	accumulator
+}
+
+pub fn part_one(strings: &Intermediate) -> Option<Solution> {
+	let bit_counts: [[usize; 2]; 12] = strings.iter().fold([[0; 2]; 12], bit_count);
+
+	let gamma_rate_bits: String = bit_counts
+		.iter()
+		.map(|[zc, oc]| {
+			if oc > zc {
+				'1'
+			} else if zc > oc {
+				'0'
+			} else {
+				panic!("zc = {}, oc = {}, neither is more common!", zc, oc);
+			}
+		})
+		.collect();
+
+	let gamma_rate: u32 = u32::from_str_radix(&gamma_rate_bits, 2).unwrap();
+
+	let epsilon_rate: u32 = !gamma_rate & 0b00001111_11111111;
+
+	Some(gamma_rate * epsilon_rate)
 }
 
 pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
