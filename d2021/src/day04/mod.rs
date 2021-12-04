a2c6a156c64fcb0ad2938792139832ac126ed36c
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-04 14:30:17 -0600

    2021/04: Make record_all_rounds return an iterator
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day04/mod.rs
+++ d2021/src/day04/mod.rs
@@ -68,26 +68,24 @@ pub fn part_one((calls, boards): &Intermediate) -> Option<Solution> {
 	Some(score as usize)
 }
 
-struct Turn<'a, 'b: 'a> {
+struct Turn<'a> {
 	call: Number,
 	winners: Vec<&'a Board>,
-	calls_so_far: &'b [Number],
+	calls_so_far: &'a [Number],
 }
 
-fn record_all_rounds<'a, 'b>(calls: &'b [Number], boards: &'b [Board]) -> Vec<Turn<'a, 'b>> {
-	let mut seen_calls: BTreeSet<Number> = BTreeSet::new();
-
-	let mut remaining_boards: HashSet<&Board> = boards.iter().collect();
-
-	calls
-		.iter()
-		.enumerate()
-		.map(|(idx, &call)| {
+fn record_all_rounds<'a>(
+	calls: &'a [Number],
+	boards: &'a [Board],
+) -> impl Iterator<Item = Turn<'a>> {
+	calls.iter().enumerate().scan(
+		(boards.iter().collect(), BTreeSet::new()),
+		|(remaining_boards, seen_calls): &mut (HashSet<&Board>, BTreeSet<Number>), (idx, &call)| {
 			seen_calls.insert(call);
 
-			let mut winners: Vec<&'b Board> = vec![];
+			let mut winners: Vec<&'a Board> = vec![];
 
-			for &board in &remaining_boards {
+			for &board in remaining_boards.iter() {
 				if board.numbers().intersection(&seen_calls).count() < 5 {
 					continue;
 				} else if let Some(_winning_move) = board.find_winning_move(&seen_calls) {
@@ -99,34 +97,34 @@ fn record_all_rounds<'a, 'b>(calls: &'b [Number], boards: &'b [Board]) -> Vec<Tu
 				remaining_boards.remove(board);
 			}
 
-			let calls_so_far = &calls[0..=idx];
+			let calls_so_far: &'a [Number] = &calls[0..=idx];
 
-			Turn {
+			Some(Turn {
 				call,
 				winners,
 				calls_so_far,
-			}
-		})
-		.collect()
+			})
+		},
+	)
 }
 
 pub fn part_two((calls, boards): &Intermediate) -> Option<Solution> {
-	let all_turns = record_all_rounds(calls, boards);
+	let all_turns: Vec<Turn> = record_all_rounds(calls, boards).collect();
 
-	let (&last_call, winning_board, seen_calls) = all_turns
+	let last_turn = all_turns
 		.iter()
 		.rfind(|Turn { winners, .. }| !winners.is_empty())
-		.map(|turn| {
-			let Turn {
-				call,
-				winners,
-				calls_so_far,
-				..
-			} = turn;
-
-			(call, winners[0], calls_so_far.iter().copied().collect())
-		})
 		.unwrap();
+
+	let Turn {
+		call,
+		winners,
+		calls_so_far,
+	} = last_turn;
+
+	let &last_call = call;
+	let winning_board = winners[0];
+	let seen_calls = calls_so_far.iter().copied().collect();
 
 	let score: u32 = winning_board.score(last_call, &seen_calls);
 
