4cb5daaf75b1257e2a99941f2dcd87afc11971c2
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-04 12:45:33 -0600

    2021/04: Implement part one
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day04/mod.rs
+++ d2021/src/day04/mod.rs
@@ -1,11 +1,182 @@
-type Intermediate = ();
+use std::{collections::BTreeSet, num::ParseIntError};
 
-pub fn parse(_input: &str) -> Intermediate {}
+type Number = u8;
 
-type Solution = ();
+#[derive(Debug)]
+pub struct Board {
+	contents: [[Number; 5]; 5],
+	winning_moves: Vec<BTreeSet<Number>>,
+	all_contents: BTreeSet<Number>,
+}
 
-pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+impl Board {
+	fn generate_winning_moves(contents: &[[Number; 5]; 5]) -> Vec<BTreeSet<Number>> {
+		let mut winning_moves = Vec::new();
+
+		for row in 0..5 {
+			let rank_set: BTreeSet<Number> = BTreeSet::from(contents[row]);
+			winning_moves.push(rank_set);
+		}
+
+		for column in 0..5 {
+			let mut file_set: BTreeSet<Number> = BTreeSet::new();
+
+			for idx in 0..5 {
+				file_set.insert(contents[idx][column]);
+			}
+
+			winning_moves.push(file_set);
+		}
+
+		winning_moves
+	}
+
+	fn check_win(&self, seen_calls: &BTreeSet<Number>) -> (bool, Option<&BTreeSet<Number>>) {
+		let winning_move = self
+			.winning_moves
+			.iter()
+			.find(|winning_move| seen_calls.is_superset(winning_move));
+
+		(winning_move.is_some(), winning_move)
+	}
+
+	fn from_contents(contents: [[Number; 5]; 5]) -> Self {
+		let winning_moves = Self::generate_winning_moves(&contents);
+
+		let all_contents: BTreeSet<Number> = contents
+			.iter()
+			.map(|row| row.iter().copied())
+			.flatten()
+			.collect();
+
+		Self {
+			contents,
+			winning_moves,
+			all_contents,
+		}
+	}
+}
+
+#[derive(Debug, thiserror::Error, PartialEq)]
+pub enum Error {
+	#[error("failed to parse a number")]
+	IntegerParse(#[from] ParseIntError),
+
+	#[error("failed to parse board")]
+	BoardParse,
+}
+
+fn parse_board_line(line: &str) -> Result<[Number; 5], Error> {
+	let result: Result<Vec<Number>, Error> = line
+		.split_ascii_whitespace()
+		.map(str::parse)
+		.collect::<Result<Vec<Number>, _>>()
+		.map_err(Error::from);
+
+	match result {
+		Ok(vec) => <[Number; 5]>::try_from(vec).or_else(|_| Err(Error::BoardParse)),
+		Err(e) => Err(e),
+	}
+}
+
+fn parse_board(board: &str) -> Result<[[Number; 5]; 5], Error> {
+	let result: Result<Vec<[Number; 5]>, Error> = board
+		.split("\n")
+		.map(parse_board_line)
+		.collect::<Result<Vec<[Number; 5]>, Error>>();
+
+	match result {
+		Ok(vec) => <[[Number; 5]; 5]>::try_from(vec).or_else(|_| Err(Error::BoardParse)),
+		Err(e) => Err(e),
+	}
+}
+
+#[cfg(test)]
+mod parse_board_line {
+	use super::{parse_board_line, Error};
+
+	#[test]
+	fn line_62_5_77_94_75() {
+		assert_eq!(parse_board_line("62  5 77 94 75"), Ok([62, 5, 77, 94, 75]));
+	}
+
+	#[test]
+	fn line_62_5_77_94() {
+		assert_eq!(parse_board_line("62  5 77 94"), Err(Error::BoardParse));
+	}
+
+	#[test]
+	fn line_62_5_77_94_asdf() {
+		assert!(parse_board_line("62  5 77 94 asdf").is_err())
+	}
+}
+
+impl core::str::FromStr for Board {
+	type Err = Error;
+
+	fn from_str(board: &str) -> Result<Self, Self::Err> {
+		parse_board(board).map(Board::from_contents)
+	}
+}
+
+type Intermediate = (Vec<Number>, Vec<Board>);
+
+pub fn parse(input: &str) -> Intermediate {
+	let mut results = input.split("\n\n");
+	let calls = results.next().unwrap();
+	let boards = results;
+
+	let calls: Vec<Number> = calls
+		.split(',')
+		.map(str::parse)
+		.filter_map(Result::ok)
+		.collect();
+
+	let boards: Vec<Board> = boards.map(str::parse).filter_map(Result::ok).collect();
+
+	(calls, boards)
+}
+
+type Solution = usize;
+
+pub fn part_one((calls, boards): &Intermediate) -> Option<Solution> {
+	let mut seen_calls = BTreeSet::new();
+
+	let mut calls = calls.iter();
+
+	let winning_board: Option<(Number, &Board)> = 'main: loop {
+		let call = calls.next();
+
+		if let Some(&call) = call {
+			seen_calls.insert(call);
+
+			for board in boards {
+				if board.all_contents.intersection(&seen_calls).count() < 5 {
+					continue;
+				} else {
+					let check = board.check_win(&seen_calls);
+
+					// The board did win!
+					if check.0 {
+						break 'main Some((call, board));
+					}
+				}
+			}
+		} else {
+			break None;
+		}
+	};
+
+	let (last_call, winning_board) = winning_board.expect("failed to find a winning board");
+
+	let score: u32 = winning_board
+		.all_contents
+		.difference(&seen_calls)
+		.map(|n| *n as u32)
+		.sum::<u32>()
+		* last_call as u32;
+
+	Some(score as usize)
 }
 
 pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
