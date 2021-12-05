1463e5c6e30a8d3f05458d61dba9adbc479fb86a
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-05 11:41:02 -0600

    2021/05: Restructure points around a Vector type
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day05/mod.rs
+++ d2021/src/day05/mod.rs
@@ -1,7 +1,7 @@
 use {
 	core::str::FromStr,
 	std::{
-		collections::{btree_map::Entry, BTreeMap, BTreeSet},
+		collections::{btree_map::Entry, BTreeMap},
 		convert::Infallible,
 	},
 };
@@ -9,7 +9,58 @@ use {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Vector(i16, i16);
 
-impl FromStr for Vector {
+impl Vector {
+	fn unit_and_steps(&self) -> (Vector, i16) {
+		let unit: (i16, i16) = (
+			if self.0 == 0 {
+				0
+			} else {
+				self.0 / self.0.abs()
+			},
+			if self.1 == 0 {
+				0
+			} else {
+				self.1 / self.1.abs()
+			},
+		);
+		let steps: i16 = if unit.0 == 0 {
+			self.1 / unit.1
+		} else {
+			self.0 / unit.0
+		};
+
+		(Vector(unit.0, unit.1), steps)
+	}
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Point(Vector);
+
+impl Point {
+	fn step(&self, v: Vector) -> impl Iterator<Item = Point> + '_ {
+		let (unit, steps) = v.unit_and_steps();
+
+		(0..=steps).map(move |i| Point(Vector(self.0 .0 + unit.0 * i, self.0 .1 + unit.1 * i)))
+	}
+}
+
+impl core::ops::Sub for Point {
+	type Output = Vector;
+
+	fn sub(self, rhs: Self) -> Self::Output {
+		Vector((self.0).0 - (rhs.0).0, (self.0).1 - (rhs.0).1)
+	}
+}
+
+impl core::ops::Add<Vector> for Point {
+	type Output = Point;
+
+	fn add(self, rhs: Vector) -> Self::Output {
+		Self(Vector(self.0 .0 + rhs.0, self.0 .1 + rhs.1))
+	}
+}
+
+impl FromStr for Point {
 	// I think you will find this FromStr quite fallible!
 	// For the input, though, this works just fine.
 	type Err = Infallible;
@@ -21,101 +72,87 @@ impl FromStr for Vector {
 		let y = split.next().map(str::parse);
 
 		match (x, y) {
-			(Some(Ok(x)), Some(Ok(y))) => Ok(Self(x, y)),
+			(Some(Ok(x)), Some(Ok(y))) => Ok(Self(Vector(x, y))),
 			_ => unreachable!(),
 		}
 	}
 }
 
 #[cfg(test)]
-mod vector {
-	use super::Vector;
+mod point {
+	use super::{Point, Vector};
 
 	#[test]
 	fn from_str_simple() {
-		assert_eq!("1,2".parse(), Ok(Vector(1, 2)))
+		assert_eq!("1,2".parse(), Ok(Point(Vector(1, 2))))
 	}
 }
 
 #[derive(Clone, Copy, Debug)]
 pub struct LineSegment {
-	a: Vector,
-	b: Vector,
+	origin: Point,
+	direction: Vector,
 }
 
 impl LineSegment {
+	fn from_points(a: Point, b: Point) -> Self {
+		let origin = a;
+		let direction = b - a;
+
+		Self { origin, direction }
+	}
+
 	fn is_horizontal(&self) -> bool {
-		self.a.1 == self.b.1
+		self.direction.1 == 0
 	}
 
 	fn is_vertical(&self) -> bool {
-		self.a.0 == self.b.0
+		self.direction.0 == 0
 	}
 
+	#[cfg(test)]
 	fn is_diagonal(&self) -> bool {
-		let (dx, dy) = (self.b.0 - self.a.0, self.b.1 - self.a.1);
+		self.direction.0 != 0 && (self.direction.1 / self.direction.0).abs() == 1
+		// let (dx, dy) = (self.b.0 - self.a.0, self.b.1 - self.a.1);
 
-		dx.abs() == dy.abs()
+		// dx.abs() == dy.abs()
 	}
 
-	fn points(&self) -> Box<dyn Iterator<Item = Vector>> {
-		if self.is_horizontal() {
-			let y = self.a.1;
-			assert_eq!(y, self.b.1);
-
-			Box::new((self.a.0..=self.b.0).map(move |x| Vector(x, y)))
-		} else if self.is_vertical() {
-			let x = self.a.0;
-			assert_eq!(x, self.b.0);
-
-			Box::new((self.a.1..=self.b.1).map(move |y| Vector(x, y)))
-		} else if self.is_diagonal() {
-			let (dir, steps): ((i16, i16), i16) = (
-				(
-					if self.b.0 - self.a.0 > 0 { 1 } else { -1 },
-					if self.b.1 - self.a.1 > 0 { 1 } else { -1 },
-				),
-				(self.b.0 - self.a.0).abs(),
-			);
-
-			let x0 = self.a.0;
-			let y0 = self.a.1;
-
-			Box::new((0..=steps).map(move |i| {
-				let x = x0 + (i * dir.0);
-				let y = y0 + (i * dir.1);
-
-				Vector(x, y)
-			}))
-		} else {
-			// Technically _definitely_ reachable, but not in the problem space.
-			unreachable!()
-		}
+	fn points(&self) -> impl Iterator<Item = Point> + '_ {
+		self.origin.step(self.direction)
 	}
 }
 
 #[test]
 fn points_diagonal_asc() {
-	let segment = LineSegment {
-		a: Vector(1, 1),
-		b: Vector(3, 3),
-	};
+	let segment = LineSegment::from_points(Point(Vector(1, 1)), Point(Vector(3, 3)));
 	assert!(segment.is_diagonal());
 
-	let points: Vec<Vector> = segment.points().collect();
-	assert_eq!(points, vec![Vector(1, 1), Vector(2, 2), Vector(3, 3)])
+	let points: Vec<Point> = segment.points().collect();
+	assert_eq!(
+		points,
+		vec![
+			Point(Vector(1, 1)),
+			Point(Vector(2, 2)),
+			Point(Vector(3, 3))
+		]
+	)
 }
 
 #[test]
 fn points_diagonal_dsc() {
-	let segment = LineSegment {
-		a: Vector(9, 7),
-		b: Vector(7, 9),
-	};
+	let segment = LineSegment::from_points(Point(Vector(9, 7)), Point(Vector(7, 9)));
 	assert!(segment.is_diagonal());
 
-	let points: Vec<Vector> = segment.points().collect();
-	assert_eq!(points, vec![Vector(9, 7), Vector(8, 8), Vector(7, 9)])
+	let points: Vec<Point> = segment.points().collect();
+	assert_eq!(
+		points,
+		vec![
+			Point(Vector(9, 7)),
+			Point(Vector(8, 8)),
+			Point(Vector(7, 9))
+		]
+	)
 }
 
 type Intermediate = Vec<LineSegment>;
@@ -124,19 +161,14 @@ impl FromStr for LineSegment {
 	type Err = Infallible;
 
 	fn from_str(line: &str) -> Result<Self, Self::Err> {
-		let points: BTreeSet<Vector> = line
+		let points: Vec<Point> = line
 			.split(" -> ")
 			.map(str::parse)
-			.collect::<Result<BTreeSet<_>, Infallible>>()?;
+			.collect::<Result<Vec<_>, Infallible>>()?;
 
 		assert_eq!(points.len(), 2);
 
-		let points: Vec<Vector> = points.into_iter().collect();
-
-		Ok(LineSegment {
-			a: points[0],
-			b: points[1],
-		})
+		Ok(LineSegment::from_points(points[0], points[1]))
 	}
 }
 
@@ -152,12 +184,12 @@ type Solution = usize;
 
 pub fn part_one(segments: &Intermediate) -> Option<Solution> {
 	let segments: Vec<LineSegment> = segments
-		.into_iter()
+		.iter()
 		.filter(|segment| segment.is_horizontal() || segment.is_vertical())
 		.copied()
 		.collect();
 
-	let mut points: BTreeMap<Vector, usize> = BTreeMap::new();
+	let mut points: BTreeMap<Point, usize> = BTreeMap::new();
 
 	for segment in segments {
 		for point in segment.points() {
@@ -174,7 +206,7 @@ pub fn part_one(segments: &Intermediate) -> Option<Solution> {
 }
 
 pub fn part_two(segments: &Intermediate) -> Option<Solution> {
-	let mut points: BTreeMap<Vector, usize> = BTreeMap::new();
+	let mut points: BTreeMap<Point, usize> = BTreeMap::new();
 
 	for segment in segments {
 		for point in segment.points() {
