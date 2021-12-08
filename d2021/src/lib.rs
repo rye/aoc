a234d8f2e197a9af6cff7f8b27a750eb39d88348
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-08 07:18:06 -0600

    2021/08: Set up and add input
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/lib.rs
+++ d2021/src/lib.rs
@@ -5,6 +5,7 @@ pub mod day04;
 pub mod day05;
 pub mod day06;
 pub mod day07;
+pub mod day08;
 
 /// Fully consumes a reader of type `std::io::Read` and produces a `String` containing all read text.
 ///
