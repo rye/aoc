caee21a53328370fcc9e95af81f81fce133397a7
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-08 07:18:19 -0600

    2021/08: Implement part one
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day08/mod.rs
+++ d2021/src/day08/mod.rs
@@ -1,11 +1,24 @@
-type Intermediate = ();
+type Intermediate<'a> = Vec<(&'a str, &'a str)>;
 
-pub fn parse(_input: &str) -> Intermediate {}
+pub fn parse<'a>(input: &'a str) -> Intermediate<'a> {
+	input
+		.lines()
+		.map(|line| line.split(" | "))
+		.map(|mut split| (split.next().unwrap(), split.next().unwrap()))
+		.collect()
+}
 
 type Solution = usize;
 
-pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+pub fn part_one(parts: &Intermediate) -> Option<Solution> {
+	Some(
+		parts
+			.iter()
+			.map(|n| n.1.split(' '))
+			.flatten()
+			.filter(|s| s.len() == 2 || s.len() == 3 || s.len() == 4 || s.len() == 7)
+			.count(),
+	)
 }
 
 pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
