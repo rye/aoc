951613f5de729f72d4cfcca3a7052c6cd6276b17
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-15 06:15:21 -0600

    2021/14: Set up initial parsing logic
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day14/mod.rs
+++ d2021/src/day14/mod.rs
@@ -1,8 +1,20 @@
-type Intermediate = ();
+type Intermediate<'input> = (&'input str, [&'input str; 100]);
 
-pub fn parse(_input: &str) -> Intermediate {}
+pub fn parse<'input>(input: &'input str) -> Intermediate<'input> {
+	let mut split = input.split("\n\n");
+	let first_line: &'input str = split.next().expect("missing template");
+	let insertion_rules: &'input str = split.next().expect("missing rules");
 
-type Solution = ();
+	let insertion_rules: [&'input str; 100] = insertion_rules
+		.lines()
+		.collect::<Vec<_>>()
+		.try_into()
+		.unwrap();
+
+	(first_line, insertion_rules)
+}
+
+type Solution = usize;
 
 pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
 	None
