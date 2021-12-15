4ff0d388e96e8c0048f762d84fe5be6d4ea2a571
Kristofer Rye <kristofer.rye@gmail.com>
2021-12-15 07:26:49 -0600

    2021/14: Implement part one
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day14/mod.rs
+++ d2021/src/day14/mod.rs
@@ -1,32 +1,134 @@
-type Intermediate<'input> = (&'input str, [[&'input str; 2]; 100]);
+use std::collections::{btree_map::Entry, BTreeMap};
+
+use itertools::Itertools;
+
+type Intermediate<'input> = (&'input str, BTreeMap<&'input str, char>);
 
 pub fn parse<'input>(input: &'input str) -> Intermediate<'input> {
 	let mut split = input.split("\n\n");
 	let first_line: &'input str = split.next().expect("missing template");
 	let insertion_rules: &'input str = split.next().expect("missing rules");
 
-	let insertion_rules: [[&'input str; 2]; 100] = insertion_rules
+	let insertion_rules: BTreeMap<&'input str, char> = insertion_rules
 		.lines()
 		.map(|line| {
-			line
-				.split(" -> ")
-				.collect::<Vec<_>>()
-				.try_into()
-				.expect("failed to collect both parts of a rule")
+			let mut parts = line.split(" -> ");
+
+			let pair = parts.next().expect("failed to get pair for matching");
+			let element: char = parts
+				.next()
+				.expect("missing element")
+				.chars()
+				.next()
+				.expect("missing insertion character");
+
+			(pair, element)
 		})
-		.collect::<Vec<[&'input str; 2]>>()
-		.try_into()
-		.expect("failed to collect 100 rules");
+		.collect();
 
 	(first_line, insertion_rules)
 }
 
 type Solution = usize;
 
-pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+fn one_step_inner(
+	(_a_idx, a_c): (usize, char),
+	(b_idx, b_c): (usize, char),
+	rules: &BTreeMap<&str, char>,
+	len: usize,
+) -> String {
+	let last = b_idx == len - 1;
+
+	let lookup_pair: String = [a_c, b_c].iter().collect();
+
+	let mut pair: Vec<char> = vec![a_c];
+
+	let lookup_pair: &str = &lookup_pair;
+
+	if let Some(&element) = rules.get(lookup_pair) {
+		pair.push(element);
+	}
+
+	if last {
+		pair.push(b_c);
+	}
+
+	pair.iter().collect()
 }
 
-pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+fn one_step(existing_polymer: &str, rules: &BTreeMap<&str, char>) -> String {
+	let len = existing_polymer.len();
+	existing_polymer
+		.chars()
+		.enumerate()
+		.tuple_windows()
+		.map(|(a, b)| one_step_inner(a, b, rules, len))
+		.collect()
+}
+
+#[test]
+fn one_step_example_nncb_nn_c() {
+	let rules = vec![("NN", 'C')].into_iter().collect();
+
+	assert_eq!(one_step("NNCB", &rules), "NCNCB");
+}
+
+#[test]
+fn one_step_example_nncb_nn_c_nc_b() {
+	let rules = vec![("NN", 'C'), ("NC", 'B')].into_iter().collect();
+	assert_eq!(one_step("NNCB", &rules), "NCNBCB");
+}
+
+#[test]
+fn one_step_example_nncb_nn_c_nc_b_cb_h() {
+	let rules = vec![("NN", 'C'), ("NC", 'B'), ("CB", 'H')]
+		.into_iter()
+		.collect();
+
+	assert_eq!(one_step("NNCB", &rules), "NCNBCHB");
+}
+
+fn statistics(string: &str) -> BTreeMap<char, usize> {
+	let mut statistics: BTreeMap<char, usize> = Default::default();
+
+	for c in string.chars() {
+		match statistics.entry(c) {
+			Entry::Occupied(mut e) => e.insert(e.get() + 1),
+			Entry::Vacant(e) => *e.insert(1),
+		};
+	}
+
+	statistics
+}
+
+pub fn part_one((template, rules): &Intermediate) -> Option<Solution> {
+	let mut template = template.to_string();
+
+	for step in 0..10 {
+		template = one_step(&template, &rules);
+		println!("Step {}: length = {}", step + 1, template.len());
+	}
+
+	let statistics = statistics(&template);
+
+	let qty_max = statistics.values().max().expect("expected a maximum");
+	let qty_min = statistics.values().min().expect("expected a minimum");
+
+	Some(qty_max - qty_min)
+}
+
+pub fn part_two((template, rules): &Intermediate) -> Option<Solution> {
+	let mut template = template.to_string();
+
+	for step in 0..40 {
+		template = one_step(&template, &rules);
+		println!("Step {}: length = {}", step + 1, template.len());
+	}
+
+	let statistics = statistics(&template);
+
+	let qty_max = statistics.values().max().expect("expected a maximum");
+	let qty_min = statistics.values().min().expect("expected a minimum");
+
+	Some(qty_max - qty_min)
 }
