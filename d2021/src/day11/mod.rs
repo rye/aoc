8b9c182e231fe86558aab19df5af6d136e6ef831
Kristofer Rye <kristofer.rye@gmail.com>
2022-03-25 11:02:37 -0500

    2021/11: Initial stab at parsing
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day11/mod.rs
+++ d2021/src/day11/mod.rs
@@ -1,8 +1,65 @@
-type Intermediate = ();
+type Intermediate = State;
 
-pub fn parse(_input: &str) -> Intermediate {}
+#[derive(Debug)]
+struct EnergyLevel(u8);
 
-type Solution = ();
+impl From<u8> for EnergyLevel {
+	fn from(u8: u8) -> Self {
+		Self(u8)
+	}
+}
+
+impl From<char> for EnergyLevel {
+	fn from(char: char) -> Self {
+		EnergyLevel(match char {
+			'0' => 0_u8,
+			'1' => 1,
+			'2' => 2,
+			'3' => 3,
+			'4' => 4,
+			'5' => 5,
+			'6' => 6,
+			'7' => 7,
+			'8' => 8,
+			'9' => 9,
+			_ => panic!("unrecognized energy level!"),
+		})
+	}
+}
+
+#[derive(Debug)]
+struct Octopus(EnergyLevel);
+
+impl From<char> for Octopus {
+	fn from(char: char) -> Self {
+		Self(char.into())
+	}
+}
+
+pub struct State {
+	octopi: [[Octopus; 10]; 10],
+}
+
+pub fn parse(input: &str) -> Intermediate {
+	let octopi: [[Octopus; 10]; 10] = input
+		.lines()
+		.map(|line| {
+			let octopi: [Octopus; 10] = line
+				.chars()
+				.map(|char| char.into())
+				.collect::<Vec<_>>()
+				.try_into()
+				.expect("failed to parse 10 octopi on a line");
+			octopi
+		})
+		.collect::<Vec<_>>()
+		.try_into()
+		.expect("failed to parse 10 lines of octopi");
+
+	State { octopi }
+}
+
+type Solution = usize;
 
 pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
 	None
