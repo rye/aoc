456def66275b595ce7760a7e5a68be0f7e4d921c
Kristofer Rye <kristofer.rye@gmail.com>
2022-03-26 09:20:07 -0500

    2021/11: Implement neighbor generation logic
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day11/mod.rs
+++ d2021/src/day11/mod.rs
@@ -63,6 +63,88 @@ pub fn parse(input: &str) -> Intermediate {
 	State { octopi }
 }
 
+fn neighbors<const N: usize>(y_0: u8, x_0: u8) -> impl Iterator<Item = (u8, u8)> {
+	assert!(N <= u8::MAX as usize);
+
+	let upper_bound = N as u8;
+
+	let y_min = y_0.saturating_sub(1);
+	let y_max = y_0.saturating_add(1).clamp(0, upper_bound - 1);
+
+	let x_min = x_0.saturating_sub(1);
+	let x_max = x_0.saturating_add(1).clamp(0, upper_bound - 1);
+
+	(y_min..=y_max)
+		.flat_map(move |y| (x_min..=x_max).map(move |x| (y, x)))
+		.filter(move |(y, x)| y != &y_0 || x != &x_0)
+}
+
+#[cfg(test)]
+mod neighbors {
+	use super::neighbors;
+
+	#[cfg(test)]
+	mod _5 {
+
+		use super::neighbors;
+
+		#[test]
+		fn corners() {
+			let mut neighbors = neighbors::<5>(0, 0);
+
+			assert_eq!(neighbors.next(), Some((0, 1)));
+			assert_eq!(neighbors.next(), Some((1, 0)));
+			assert_eq!(neighbors.next(), Some((1, 1)));
+			assert_eq!(neighbors.next(), None);
+		}
+
+		#[test]
+		fn inner_center() {
+			let mut neighbors = neighbors::<5>(2, 2);
+
+			assert_eq!(neighbors.next(), Some((1, 1)));
+			assert_eq!(neighbors.next(), Some((1, 2)));
+			assert_eq!(neighbors.next(), Some((1, 3)));
+			assert_eq!(neighbors.next(), Some((2, 1)));
+			// assert_eq!(neighbors.next(), Some((2, 2)));
+			assert_eq!(neighbors.next(), Some((2, 3)));
+			assert_eq!(neighbors.next(), Some((3, 1)));
+			assert_eq!(neighbors.next(), Some((3, 2)));
+			assert_eq!(neighbors.next(), Some((3, 3)));
+			assert_eq!(neighbors.next(), None);
+		}
+
+		#[test]
+		fn inner_off_center() {
+			let mut neighbors = neighbors::<5>(3, 3);
+
+			assert_eq!(neighbors.next(), Some((2, 2)));
+			assert_eq!(neighbors.next(), Some((2, 3)));
+			assert_eq!(neighbors.next(), Some((2, 4)));
+			assert_eq!(neighbors.next(), Some((3, 2)));
+			// assert_eq!(neighbors.next(), Some((3, 3)));
+			assert_eq!(neighbors.next(), Some((3, 4)));
+			assert_eq!(neighbors.next(), Some((4, 2)));
+			assert_eq!(neighbors.next(), Some((4, 3)));
+			assert_eq!(neighbors.next(), Some((4, 4)));
+			assert_eq!(neighbors.next(), None);
+		}
+
+		#[test]
+		fn edge_off_center() {
+			let mut neighbors = neighbors::<5>(3, 4);
+
+			assert_eq!(neighbors.next(), Some((2, 3)));
+			assert_eq!(neighbors.next(), Some((2, 4)));
+			assert_eq!(neighbors.next(), Some((3, 3)));
+			// assert_eq!(neighbors.next(), Some((3, 4)));
+			assert_eq!(neighbors.next(), Some((4, 3)));
+			assert_eq!(neighbors.next(), Some((4, 4)));
+			assert_eq!(neighbors.next(), None);
+		}
+	}
+}
+
 fn tick<const N: usize>(state: &mut State<N>) -> usize {
 	0
 }
