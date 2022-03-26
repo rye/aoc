2e2f7bb5e5ce50a7b2c0f12f8c61ed326508c9b6
Kristofer Rye <kristofer.rye@gmail.com>
2022-03-26 10:18:16 -0500

    2021/11: Remove duplicate module in path name
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day11/neighbors.rs
+++ d2021/src/day11/neighbors.rs
@@ -13,69 +13,62 @@ pub fn neighbors<const N: usize>(y_0: u8, x_0: u8) -> impl Iterator<Item = (u8, 
 		.flat_map(move |y| (x_min..=x_max).map(move |x| (y, x)))
 		.filter(move |(y, x)| y != &y_0 || x != &x_0)
 }
-
 #[cfg(test)]
-mod neighbors {
+mod _5 {
 	use super::neighbors;
 
-	#[cfg(test)]
-	mod _5 {
+	#[test]
+	fn corners() {
+		let mut neighbors = neighbors::<5>(0, 0);
 
-		use super::neighbors;
+		assert_eq!(neighbors.next(), Some((0, 1)));
+		assert_eq!(neighbors.next(), Some((1, 0)));
+		assert_eq!(neighbors.next(), Some((1, 1)));
+		assert_eq!(neighbors.next(), None);
+	}
 
-		#[test]
-		fn corners() {
-			let mut neighbors = neighbors::<5>(0, 0);
+	#[test]
+	fn inner_center() {
+		let mut neighbors = neighbors::<5>(2, 2);
 
-			assert_eq!(neighbors.next(), Some((0, 1)));
-			assert_eq!(neighbors.next(), Some((1, 0)));
-			assert_eq!(neighbors.next(), Some((1, 1)));
-			assert_eq!(neighbors.next(), None);
-		}
+		assert_eq!(neighbors.next(), Some((1, 1)));
+		assert_eq!(neighbors.next(), Some((1, 2)));
+		assert_eq!(neighbors.next(), Some((1, 3)));
+		assert_eq!(neighbors.next(), Some((2, 1)));
+		// assert_eq!(neighbors.next(), Some((2, 2)));
+		assert_eq!(neighbors.next(), Some((2, 3)));
+		assert_eq!(neighbors.next(), Some((3, 1)));
+		assert_eq!(neighbors.next(), Some((3, 2)));
+		assert_eq!(neighbors.next(), Some((3, 3)));
+		assert_eq!(neighbors.next(), None);
+	}
 
-		#[test]
-		fn inner_center() {
-			let mut neighbors = neighbors::<5>(2, 2);
+	#[test]
+	fn inner_off_center() {
+		let mut neighbors = neighbors::<5>(3, 3);
 
-			assert_eq!(neighbors.next(), Some((1, 1)));
-			assert_eq!(neighbors.next(), Some((1, 2)));
-			assert_eq!(neighbors.next(), Some((1, 3)));
-			assert_eq!(neighbors.next(), Some((2, 1)));
-			// assert_eq!(neighbors.next(), Some((2, 2)));
-			assert_eq!(neighbors.next(), Some((2, 3)));
-			assert_eq!(neighbors.next(), Some((3, 1)));
-			assert_eq!(neighbors.next(), Some((3, 2)));
-			assert_eq!(neighbors.next(), Some((3, 3)));
-			assert_eq!(neighbors.next(), None);
-		}
+		assert_eq!(neighbors.next(), Some((2, 2)));
+		assert_eq!(neighbors.next(), Some((2, 3)));
+		assert_eq!(neighbors.next(), Some((2, 4)));
+		assert_eq!(neighbors.next(), Some((3, 2)));
+		// assert_eq!(neighbors.next(), Some((3, 3)));
+		assert_eq!(neighbors.next(), Some((3, 4)));
+		assert_eq!(neighbors.next(), Some((4, 2)));
+		assert_eq!(neighbors.next(), Some((4, 3)));
+		assert_eq!(neighbors.next(), Some((4, 4)));
+		assert_eq!(neighbors.next(), None);
+	}
 
-		#[test]
-		fn inner_off_center() {
-			let mut neighbors = neighbors::<5>(3, 3);
+	#[test]
+	fn edge_off_center() {
+		let mut neighbors = neighbors::<5>(3, 4);
 
-			assert_eq!(neighbors.next(), Some((2, 2)));
-			assert_eq!(neighbors.next(), Some((2, 3)));
-			assert_eq!(neighbors.next(), Some((2, 4)));
-			assert_eq!(neighbors.next(), Some((3, 2)));
-			// assert_eq!(neighbors.next(), Some((3, 3)));
-			assert_eq!(neighbors.next(), Some((3, 4)));
-			assert_eq!(neighbors.next(), Some((4, 2)));
-			assert_eq!(neighbors.next(), Some((4, 3)));
-			assert_eq!(neighbors.next(), Some((4, 4)));
-			assert_eq!(neighbors.next(), None);
-		}
-
-		#[test]
-		fn edge_off_center() {
-			let mut neighbors = neighbors::<5>(3, 4);
-
-			assert_eq!(neighbors.next(), Some((2, 3)));
-			assert_eq!(neighbors.next(), Some((2, 4)));
-			assert_eq!(neighbors.next(), Some((3, 3)));
-			// assert_eq!(neighbors.next(), Some((3, 4)));
-			assert_eq!(neighbors.next(), Some((4, 3)));
-			assert_eq!(neighbors.next(), Some((4, 4)));
-			assert_eq!(neighbors.next(), None);
-		}
+		assert_eq!(neighbors.next(), Some((2, 3)));
+		assert_eq!(neighbors.next(), Some((2, 4)));
+		assert_eq!(neighbors.next(), Some((3, 3)));
+		// assert_eq!(neighbors.next(), Some((3, 4)));
+		assert_eq!(neighbors.next(), Some((4, 3)));
+		assert_eq!(neighbors.next(), Some((4, 4)));
+		assert_eq!(neighbors.next(), None);
 	}
 }
