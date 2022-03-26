60d1b50541490dfc243dbc98685eac625cb15990
Kristofer Rye <kristofer.rye@gmail.com>
2022-03-26 09:35:45 -0500

    2021/11: Add stubs for tests of _10 examples as well
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2021/src/day11/mod.rs
+++ d2021/src/day11/mod.rs
@@ -259,6 +259,54 @@ mod tick {
 			assert_eq!(state, state_2());
 		}
 	}
+
+	#[cfg(test)]
+	mod _10 {
+		use super::{tick, EnergyLevel, Octopus, State};
+
+		fn state_0() -> State<10> {
+			State {
+				octopi: [
+					state![5 4 8 3 1 4 3 2 2 3],
+					state![2 7 4 5 8 5 4 7 1 1],
+					state![5 2 6 4 5 5 6 1 7 3],
+					state![6 1 4 1 3 3 6 1 4 6],
+					state![6 3 5 7 3 8 5 4 7 8],
+					state![4 1 6 7 5 2 4 6 4 5],
+					state![2 1 7 6 8 4 1 7 2 1],
+					state![6 8 8 2 8 8 1 1 3 4],
+					state![4 8 4 6 8 4 8 5 5 4],
+					state![5 2 8 3 7 5 1 5 2 6],
+				],
+			}
+		}
+
+		fn state_1() -> State<10> {
+			State {
+				octopi: [
+					state![6 5 9 4 2 5 4 3 3 4],
+					state![3 8 5 6 9 6 5 8 2 2],
+					state![6 3 7 5 6 6 7 2 8 4],
+					state![7 2 5 2 4 4 7 2 5 7],
+					state![7 4 6 8 4 9 6 5 8 9],
+					state![5 2 7 8 6 3 5 7 5 6],
+					state![3 2 8 7 9 5 2 8 3 2],
+					state![7 9 9 3 9 9 2 2 4 5],
+					state![5 9 5 7 9 5 9 6 6 5],
+					state![6 3 9 4 8 6 2 6 3 7],
+				],
+			}
+		}
+
+		#[test]
+		fn tick_flow() {
+			let mut state = state_0();
+			assert_eq!(state, state_0());
+			assert_eq!(tick(&mut state), 0_usize);
+			assert_eq!(state, state_1());
+			assert_eq!(tick(&mut state), 35_usize);
+		}
+	}
 }
 
 type Solution = usize;
