d5f5b8b5d45759a5c2a9e1d6a0ae824e5c03e89a
Kristofer Rye <kristofer.rye@gmail.com>
2022-10-30 12:25:59 -0500

    Switch back from phf in favor of modularizing
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ daocutil/src/lib.rs
@@ -0,0 +1,133 @@
+use std::num::ParseIntError;
+
+pub type Solver = fn(&str) -> Result<(), Box<dyn std::error::Error>>;
+
+pub fn string_from(mut read: impl std::io::Read) -> std::io::Result<String> {
+	let mut buf: String = String::new();
+	read.read_to_string(&mut buf)?;
+	Ok(buf)
+}
+
+fn parse_just_numbers(str: &str) -> Result<u32, ParseIntError> {
+	str.matches(char::is_numeric).collect::<String>().parse()
+}
+
+pub fn parse_day_identifier(str: &str) -> Option<u32> {
+	match (str.parse(), parse_just_numbers(str)) {
+		(Ok(u32), _) => Some(u32),
+		(Err(_), Ok(u32)) => Some(u32),
+		(_, _) => None,
+	}
+}
+
+#[macro_export]
+macro_rules! day_solver {
+	($place:path, $fn_name:ident) => {
+		fn $fn_name(data: &str) -> Result<(), Box<dyn std::error::Error>> {
+			use $place::{parse, part_one, part_two, Intermediate};
+
+			let intermediate: Intermediate = parse(data)?;
+
+			if let Some(part_one) = part_one(&intermediate) {
+				println!("Part One: {}", part_one);
+			}
+
+			if let Some(part_two) = part_two(&intermediate) {
+				println!("Part Two: {}", part_two);
+			}
+
+			Ok(())
+		}
+	};
+}
+
+#[macro_export]
+macro_rules! generate_solvers {
+	($($n:literal => $place:path as $fn_name:ident),* $(,)?) => {
+		{
+			$(
+				daocutil::day_solver!($place, $fn_name);
+			)*
+
+			{
+				let mut map: std::collections::HashMap<u32, daocutil::Solver> = std::collections::HashMap::new();
+
+				$(
+					map.insert($n, $fn_name);
+				)*
+
+				map
+			}
+		}
+	};
+
+}
+
+#[macro_export]
+macro_rules! generate_solvers_from {
+	($place:path) => {
+		{
+			use $place as base;
+
+			daocutil::generate_solvers![
+				1_u32 => base::day01 as day01,
+				2_u32 => base::day02 as day02,
+				3_u32 => base::day03 as day03,
+				4_u32 => base::day04 as day04,
+				5_u32 => base::day05 as day05,
+				6_u32 => base::day06 as day06,
+				7_u32 => base::day07 as day07,
+				8_u32 => base::day08 as day08,
+				9_u32 => base::day09 as day09,
+				10_u32 => base::day10 as day10,
+				11_u32 => base::day11 as day11,
+				12_u32 => base::day12 as day12,
+				13_u32 => base::day13 as day13,
+				14_u32 => base::day14 as day14,
+				15_u32 => base::day15 as day15,
+				16_u32 => base::day16 as day16,
+				17_u32 => base::day17 as day17,
+				18_u32 => base::day18 as day18,
+				19_u32 => base::day19 as day19,
+				20_u32 => base::day20 as day20,
+				21_u32 => base::day21 as day21,
+				22_u32 => base::day22 as day22,
+				23_u32 => base::day23 as day23,
+				24_u32 => base::day24 as day24,
+				25_u32 => base::day25 as day25,
+			]
+		}
+	};
+}
+
+#[macro_export]
+macro_rules! generate_solution_hub_from {
+	($place:path) => {
+		fn main() -> Result<(), Box<dyn std::error::Error>> {
+			let solvers = daocutil::generate_solvers_from!($place);
+
+			let mut args = std::env::args();
+
+			let _ = args.next();
+
+			if let Some(ident) = args.next() {
+				if let Some(ident) = daocutil::parse_day_identifier(&ident) {
+					if let Some(handler) = solvers.get(&ident) {
+						let data: String = match args.next() {
+							Some(filename) => daocutil::string_from(std::fs::File::open(filename)?)?,
+							None => daocutil::string_from(std::io::stdin())?,
+						};
+
+						handler(&data)?;
+					} else {
+						println!("Day has no handler: {}", ident);
+					}
+				} else {
+					println!("Unknown day identifier: {}", ident);
+				}
+			}
+
+			Ok(())
+		}
+	};
+}
