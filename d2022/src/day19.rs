6ac215427a33192933c7cffede2b8ded6f9baceb
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-01 06:34:23 -0600

    2022: Fix all stubs to prevent pedantic clippy lints
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day19.rs
+++ d2022/src/day19.rs
@@ -1,14 +1,17 @@
 pub type Intermediate = ();
 pub type Output = u32;
 
+/// # Errors
 pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
 	Ok(())
 }
 
+#[must_use]
 pub fn part_one(_intermediate: &Intermediate) -> Option<Output> {
 	None
 }
 
+#[must_use]
 pub fn part_two(_intermediate: &Intermediate) -> Option<Output> {
 	None
 }
