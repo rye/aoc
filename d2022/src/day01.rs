172e48703821a41570d6309c8aff84290067ff41
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-01 06:06:57 -0600

    2022/01: Implement Part One
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day01.rs
+++ d2022/src/day01.rs
@@ -1,12 +1,42 @@
-pub type Intermediate = ();
-pub type Output = u32;
+use {
+	core::{num::ParseIntError, str::FromStr},
+	std::collections::BTreeSet,
+};
 
-pub fn parse(_data: &str) -> Result<Intermediate, Box<dyn std::error::Error>> {
-	Ok(())
+pub struct Elf {
+	snacks: Vec<u32>,
 }
 
-pub fn part_one(_intermediate: &Intermediate) -> Option<Output> {
-	None
+impl Elf {
+	fn calorie_total(&self) -> u32 {
+		self.snacks.iter().sum()
+	}
+}
+
+impl FromStr for Elf {
+	type Err = ParseIntError;
+
+	fn from_str(str: &str) -> Result<Self, Self::Err> {
+		let snacks = str
+			.lines()
+			.map(|line| line.parse())
+			.collect::<Result<Vec<u32>, _>>()?;
+
+		Ok(Self { snacks })
+	}
+}
+
+pub type Intermediate = Vec<Elf>;
+pub type Output = u32;
+
+pub fn parse(str: &str) -> Result<Intermediate, impl std::error::Error> {
+	str.split("\n\n").map(str::parse).collect()
+}
+
+pub fn part_one(elves: &Intermediate) -> Option<Output> {
+	let elf_carrying_totals: BTreeSet<u32> = elves.iter().map(|elf| elf.calorie_total()).collect();
+
+	elf_carrying_totals.last().copied()
 }
 
 pub fn part_two(_intermediate: &Intermediate) -> Option<Output> {
