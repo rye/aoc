d7f6489fe05fe9941b46a951d95bfc9d5104292d
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-02 05:45:17 -0600

    2022/02: Implement rudimentary parsing for input
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day02.rs
+++ d2022/src/day02.rs
@@ -1,9 +1,56 @@
-pub type Intermediate = ();
+use core::{convert::Infallible, str::FromStr};
+
+pub enum Move {
+	Rock,
+	Paper,
+	Scissors,
+}
+
+impl FromStr for Move {
+	type Err = Infallible;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		match s {
+			"X" => Ok(Self::Rock),
+			"Y" => Ok(Self::Paper),
+			"Z" => Ok(Self::Scissors),
+			"A" => Ok(Self::Rock),
+			"B" => Ok(Self::Paper),
+			"C" => Ok(Self::Scissors),
+			_ => unreachable!(),
+		}
+	}
+}
+
+pub struct StrategyPart(Move, Move);
+
+impl FromStr for StrategyPart {
+	type Err = Infallible;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let mut s = s.split(' ');
+
+		let opponent_play = s.next().expect("no opponent play?");
+		let my_play = s.next().expect("no self play?");
+
+		let opponent_play: Move = opponent_play.parse().expect("unrecognized?");
+		let my_play: Move = my_play.parse().expect("unrecognized?");
+
+		Ok(Self(opponent_play, my_play))
+	}
+}
+
+pub type Intermediate = Vec<StrategyPart>;
 pub type Output = u32;
 
 /// # Errors
-pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
-	Ok(())
+pub fn parse(str: &str) -> anyhow::Result<Intermediate> {
+	Ok(
+		str
+			.lines()
+			.map(str::parse)
+			.collect::<Result<Intermediate, _>>()?,
+	)
 }
 
 #[must_use]
