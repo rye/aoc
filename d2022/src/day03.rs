ed8994cd0aed33a29c2292ec617e52725b2ea37f
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-03 05:58:11 -0600

    2022/03: Implement Part One
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day03.rs
+++ d2022/src/day03.rs
@@ -1,14 +1,63 @@
-pub type Intermediate = ();
+use std::collections::HashSet;
+
+pub type Intermediate<'a> = Vec<(&'a str, &'a str)>;
 pub type Output = u32;
 
 /// # Errors
-pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
-	Ok(())
+pub fn parse(str: &str) -> anyhow::Result<Intermediate> {
+	Ok(
+		str
+			.lines()
+			.map(|line| line.split_at(line.len() / 2))
+			.collect(),
+	)
+}
+
+fn item_priority(item: char) -> Option<u32> {
+	u8::try_from(item).ok().map(|byte| match byte {
+		b'a'..=b'z' => ((byte - b'a') + 1) as u32,
+		b'A'..=b'Z' => ((byte - b'A') + 26 + 1) as u32,
+		_ => unreachable!(),
+	})
+}
+
+#[cfg(test)]
+mod item_priority {
+	use super::item_priority;
+
+	#[test]
+	fn item_priority_a_lc() {
+		assert_eq!(item_priority('a'), Some(1));
+	}
+
+	#[test]
+	fn item_priority_z_lc() {
+		assert_eq!(item_priority('z'), Some(26));
+	}
+
+	#[test]
+	fn item_priority_a_uc() {
+		assert_eq!(item_priority('A'), Some(27));
+	}
+
+	#[test]
+	fn item_priority_z_uc() {
+		assert_eq!(item_priority('Z'), Some(52));
+	}
 }
 
 #[must_use]
-pub fn part_one(_intermediate: &Intermediate) -> Option<Output> {
-	None
+pub fn part_one(compartment_pairs: &Intermediate) -> Option<Output> {
+	let mut sum = 0_u32;
+
+	for (compartment_a, compartment_b) in compartment_pairs {
+		let priorities_a: HashSet<u32> = compartment_a.chars().filter_map(item_priority).collect();
+		let priorities_b: HashSet<u32> = compartment_b.chars().filter_map(item_priority).collect();
+
+		sum += priorities_a.intersection(&priorities_b).sum::<u32>();
+	}
+
+	Some(sum)
 }
 
 #[must_use]
