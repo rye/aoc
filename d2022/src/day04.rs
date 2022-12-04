5dc93fcecc11906bbcfb5d3ce22b1c2aaf299558
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-04 08:00:08 -0600

    2022/04: Implement basic parsing
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day04.rs
+++ d2022/src/day04.rs
@@ -1,9 +1,33 @@
-pub type Intermediate = ();
+use core::{convert::Infallible, num::ParseIntError, ops::RangeInclusive, str::FromStr};
+
+pub type Intermediate = Vec<(Assignment, Assignment)>;
 pub type Output = u32;
 
+#[derive(Debug)]
+pub struct Assignment(RangeInclusive<u32>);
+
+impl FromStr for Assignment {
+	type Err = ParseIntError;
+
+	fn from_str(str: &str) -> Result<Self, Self::Err> {
+		let split: Vec<&str> = str.split('-').collect();
+
+		Ok(Self(split[0].parse()?..=split[1].parse()?))
+	}
+}
+
 /// # Errors
-pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
-	Ok(())
+pub fn parse(str: &str) -> anyhow::Result<Intermediate> {
+	let intermediate = str
+		.lines()
+		.map(|line| {
+			let parts: Vec<&str> = line.split(',').collect();
+
+			(parts[0].parse().unwrap(), parts[1].parse().unwrap())
+		})
+		.collect();
+
+	Ok(intermediate)
 }
 
 #[must_use]
