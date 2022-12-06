55ba76ff58f125b297d7dd045437a70b03fa4399
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-06 06:59:47 -0600

    2022/06: Use map(..).find_map(..) over find(..).map()
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day06.rs
+++ d2022/src/day06.rs
@@ -12,14 +12,18 @@ fn find_end_of_first_non_identical_seq<T: Copy + Hash + Eq>(
 	length: usize,
 	slice: &[T],
 ) -> Option<usize> {
+	// Loop over all the windows, enumerating the _start-of-window_ indices as we go.
 	slice
 		.windows(length)
 		.enumerate()
-		.find(|(_idx, window)| {
-			let set: HashSet<T> = window.iter().copied().collect();
-			set.len() == length
+		// Count the number of unique elements in T and reduce the iterator to (idx, unique element count).
+		.map(|(idx, window)| (idx, window.iter().copied().collect::<HashSet<T>>().len()))
+		// Look for the idx of the first window that has a unique number of elements matching
+		// our desired window length, then return the idx following the end of that window.
+		.find_map(|(idx, unique_count)| match unique_count {
+			sz if sz == length => Some(idx + sz),
+			_ => None,
 		})
-		.map(|(idx, _window)| idx + length)
 }
 
 #[must_use]
