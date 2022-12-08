92063520135b48f27c166209f354a0dd7fad4c6f
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-08 05:57:37 -0600

    2022/08: Add input and parse to Vec<Vec<u8>>
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day08.rs
+++ d2022/src/day08.rs
@@ -1,14 +1,35 @@
-pub type Intermediate = ();
+pub type Intermediate = Vec<Vec<u8>>;
 pub type Output = u32;
 
 /// # Errors
-pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
-	Ok(())
+pub fn parse(input: &str) -> anyhow::Result<Intermediate> {
+	Ok(
+		input
+			.lines()
+			.map(|line| {
+				line
+					.chars()
+					.filter_map(|char| char.to_digit(10))
+					.filter_map(|u32| u8::try_from(u32).ok())
+					.collect()
+			})
+			.collect(),
+	)
 }
 
 #[must_use]
-pub fn part_one(_intermediate: &Intermediate) -> Option<Output> {
+pub fn part_one(lines: &Intermediate) -> Option<Output> {
 	None
+}
+
+#[test]
+fn part_one_example() {
+	daocutil::test_example!(
+		"30373\n25512\n65332\n33549\n35390",
+		part_one,
+		parse,
+		Some(21)
+	);
 }
 
 #[must_use]
