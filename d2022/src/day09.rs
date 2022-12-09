fbc2936db65c417790402537634a7dedcbdae7c9
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-09 06:26:27 -0600

    2022/09: Write a test for Part One
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day09.rs
+++ d2022/src/day09.rs
@@ -1,5 +1,5 @@
 pub type Intermediate = ();
-pub type Output = u32;
+pub type Output = usize;
 
 /// # Errors
 pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
@@ -9,6 +9,12 @@ pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
 #[must_use]
 pub fn part_one(_intermediate: &Intermediate) -> Option<Output> {
 	None
+}
+
+#[test]
+fn part_one_example() {
+	let input = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2";
+	daocutil::test_example!(input, part_one, parse, Some(13));
 }
 
 #[must_use]
