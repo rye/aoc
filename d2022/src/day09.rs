fa23278d845676d65bd36235fa29344dd7d18c7a
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-13 14:05:10 -0600

    2022/09: Implement parsin
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day09.rs
+++ d2022/src/day09.rs
@@ -1,9 +1,55 @@
-pub type Intermediate = ();
+use core::{convert::Infallible, str::FromStr};
+
+#[derive(Default)]
+pub struct State {
+	start: (i32, i32),
+	head_pos: (i32, i32),
+	tail_pos: (i32, i32),
+}
+
+enum Direction {
+	Up,
+	Down,
+	Left,
+	Right,
+}
+
+impl FromStr for Move {
+	type Err = Infallible;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let direction = match &s[0..1] {
+			"D" => Direction::Down,
+			"U" => Direction::Up,
+			"L" => Direction::Left,
+			"R" => Direction::Right,
+			_ => unreachable!(),
+		};
+
+		let size: u16 = *&s[2..].parse().expect("failure");
+
+		Ok(Move { direction, size })
+	}
+}
+
+pub struct Move {
+	direction: Direction,
+	size: u16,
+}
+
+pub type Intermediate = (State, Vec<Move>);
 pub type Output = usize;
 
 /// # Errors
-pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
-	Ok(())
+pub fn parse(input: &str) -> anyhow::Result<Intermediate> {
+	let state = State::default();
+
+	let moves: Vec<Move> = input
+		.lines()
+		.map(str::parse)
+		.collect::<Result<Vec<Move>, _>>()?;
+
+	Ok((state, moves))
 }
 
 #[must_use]
