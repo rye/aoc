30ed17d013ee46f3734bf638144acb92dc95823c
Kristofer Rye <kristofer.rye@gmail.com>
2022-12-18 11:16:40 -0600

    2022/10: Add parsing and example input test
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2022/src/day10.rs
+++ d2022/src/day10.rs
@@ -1,15 +1,47 @@
-pub type Intermediate = ();
+use core::{convert::Infallible, num::ParseIntError, str::FromStr};
+
+pub enum Instruction {
+	Noop,
+	Addx(i32),
+}
+
+impl FromStr for Instruction {
+	type Err = ParseIntError;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		match &s[0..4] {
+			"noop" => Ok(Self::Noop),
+			"addx" => s[5..].parse().map(Self::Addx),
+			_ => unreachable!(),
+		}
+	}
+}
+
+pub type Intermediate = Vec<Instruction>;
 pub type Output = u32;
 
 /// # Errors
-pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
-	Ok(())
+pub fn parse(input: &str) -> anyhow::Result<Intermediate> {
+	Ok(
+		input
+			.lines()
+			.map(str::parse)
+			.collect::<Result<Vec<_>, _>>()?,
+	)
 }
 
 #[must_use]
 pub fn part_one(_intermediate: &Intermediate) -> Option<Output> {
 	None
 }
+
+daocutil::test_example!(
+	part_one_example,
+	parse,
+	part_one,
+	include_str!("examples/day10-longer"),
+	Some(13140)
+);
 
 #[must_use]
 pub fn part_two(_intermediate: &Intermediate) -> Option<Output> {
