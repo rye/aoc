572b1d1ad6b88ee1f8e9fe0cb5a327486f4db36c
Kristofer Rye <kristofer.rye@gmail.com>
2023-02-06 19:37:43 -0600

    2017: Initialize
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ d2017/src/day21.rs
@@ -0,0 +1,14 @@
+pub type Intermediate = ();
+pub type Solution = usize;
+
+pub fn parse(_input: &str) -> anyhow::Result<Intermediate> {
+	Ok(())
+}
+
+pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
+	None
+}
+
+pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
+	None
+}
