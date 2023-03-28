86a4f5b82a1cab3eadbb809d33a7fee19f290cdc
Kristofer Rye <kristofer.rye@gmail.com>
2023-03-27 20:18:41 -0500

    2017/03: Refactor to use Iterator
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2017/src/day03.rs
+++ d2017/src/day03.rs
@@ -12,65 +12,63 @@ daocutil::test_example!(part_one_12, parse, part_one, "12", Some(3));
 daocutil::test_example!(part_one_23, parse, part_one, "23", Some(2));
 daocutil::test_example!(part_one_1024, parse, part_one, "1024", Some(31));
 
-fn gen_grid(size: usize) -> Vec<(i32, i32)> {
-	let mut set: BTreeSet<(i32, i32)> = BTreeSet::new();
-	let mut vec = Vec::with_capacity(size + 8);
-	let mut dir = (1, 0);
+struct GridIter {
+	set: BTreeSet<(i32, i32)>,
+	last: (i32, i32),
+	dir: (i32, i32),
+}
 
-	set.insert((0, 0));
-	vec.push((0, 0));
-
-	while vec.len() < size {
-		let most_recent = vec.last().unwrap();
-		let next = (most_recent.0 + dir.0, most_recent.1 + dir.1);
-
-		assert!(!set.contains(&next));
-
-		vec.push(next);
-		set.insert(next);
-
-		if !set.contains(&(next.0 + -dir.1, next.1 + dir.0)) {
-			dir = (-dir.1, dir.0);
+impl Default for GridIter {
+	fn default() -> Self {
+		let mut set = BTreeSet::new();
+		set.insert((0, 0));
+		Self {
+			set,
+			last: (0, 0),
+			dir: (1, 0),
 		}
 	}
+}
 
-	vec
+impl Iterator for GridIter {
+	type Item = (i32, i32);
+
+	fn next(&mut self) -> Option<Self::Item> {
+		let cur = self.last;
+		let next = (self.last.0 + self.dir.0, self.last.1 + self.dir.1);
+		assert!(!self.set.contains(&next));
+
+		self.set.insert(next);
+		self.last = next;
+
+		if !self
+			.set
+			.contains(&(next.0 + -self.dir.1, next.1 + self.dir.0))
+		{
+			self.dir = (-self.dir.1, self.dir.0);
+		}
+
+		Some(cur)
+	}
 }
 
 #[test]
-fn gen_grid_1() {
-	assert_eq!(gen_grid(1), vec![(0, 0)]);
-}
-
-#[test]
-fn gen_grid_2() {
-	assert_eq!(gen_grid(2), vec![(0, 0), (1, 0)]);
-}
-
-#[test]
-fn gen_grid_3() {
-	assert_eq!(gen_grid(3), vec![(0, 0), (1, 0), (1, 1)]);
-}
-
-#[test]
-fn gen_grid_4() {
-	assert_eq!(gen_grid(4), vec![(0, 0), (1, 0), (1, 1), (0, 1)]);
-}
-
-#[test]
-fn gen_grid_5() {
-	assert_eq!(gen_grid(5), vec![(0, 0), (1, 0), (1, 1), (0, 1), (-1, 1)]);
-}
-
-#[test]
-fn gen_grid_23() {
-	assert_eq!(gen_grid(23)[22], (0, -2));
+fn gen_grid_iter() {
+	let mut gen_grid = GridIter::default();
+	assert_eq!(gen_grid.next(), Some((0, 0)));
+	assert_eq!(gen_grid.next(), Some((1, 0)));
+	assert_eq!(gen_grid.next(), Some((1, 1)));
+	assert_eq!(gen_grid.next(), Some((0, 1)));
+	assert_eq!(gen_grid.next(), Some((-1, 1)));
+	assert_eq!(gen_grid.next(), Some((-1, 0)));
+	assert_eq!(gen_grid.next(), Some((-1, -1)));
+	assert_eq!(gen_grid.next(), Some((0, -1)));
 }
 
 pub fn part_one(input: &Intermediate) -> Option<Solution> {
-	let grid = gen_grid(*input);
+	let nth = GridIter::default().nth(*input - 1);
 
-	match grid.get(*input - 1) {
+	match nth {
 		Some((x, y)) => Some(x.abs() as usize + y.abs() as usize),
 		_ => None,
 	}
