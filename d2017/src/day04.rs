71b3470b4fdad184b76cd29e2a944a05eb8117bd
Kristofer Rye <kristofer.rye@gmail.com>
2023-04-02 12:17:14 -0500

    2017/04: Implement Part One
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2017/src/day04.rs
+++ d2017/src/day04.rs
@@ -1,12 +1,50 @@
-pub type Intermediate = ();
+use std::collections::BTreeSet;
+
+pub type Intermediate<'input> = Vec<&'input str>;
 pub type Solution = usize;
 
-pub fn parse(_input: &str) -> anyhow::Result<Intermediate> {
+fn validate_password(password: &str) -> Result<(), String> {
+	let words = password.split_whitespace();
+
+	let mut seen_set = BTreeSet::default();
+
+	for word in words {
+		if seen_set.contains(word) {
+			return Err(format!("password contains word {word} more than once"));
+		} else {
+			seen_set.insert(word);
+		}
+	}
+
 	Ok(())
 }
 
-pub fn part_one(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+fn password_is_valid(password: &str) -> bool {
+	validate_password(password).is_ok()
+}
+
+#[test]
+fn validate_password_correct() {
+	assert_eq!(Ok(()), validate_password("aa bb cc dd ee"));
+	assert_eq!(
+		Err(format!("password contains word aa more than once")),
+		validate_password("aa bb cc dd aa")
+	);
+	assert_eq!(Ok(()), validate_password("aa bb cc dd aaa"));
+}
+
+pub fn parse(input: &str) -> anyhow::Result<Intermediate> {
+	let passwords = input.lines().collect();
+	Ok(passwords)
+}
+
+pub fn part_one(passwords: &Intermediate) -> Option<Solution> {
+	Some(
+		passwords
+			.iter()
+			.filter(|password| password_is_valid(&password))
+			.count(),
+	)
 }
 
 pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
