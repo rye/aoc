40ac7e43a6a9b085c5573bf6b9fc0b3172911583
Kristofer Rye <kristofer.rye@gmail.com>
2023-04-02 12:31:11 -0500

    2017/04: Implement Part Two
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2017/src/day04.rs
+++ d2017/src/day04.rs
@@ -1,38 +1,94 @@
-use std::collections::BTreeSet;
+use std::{alloc::System, collections::BTreeSet};
 
 pub type Intermediate<'input> = Vec<&'input str>;
 pub type Solution = usize;
 
-fn validate_password(password: &str) -> Result<(), String> {
+enum SystemPolicy {
+	PartOne,
+	PartTwo,
+}
+
+fn validate_password(password: &str, policy: SystemPolicy) -> Result<(), String> {
 	let words = password.split_whitespace();
 
-	let mut seen_set = BTreeSet::default();
+	match policy {
+		SystemPolicy::PartOne => {
+			let mut seen_set = BTreeSet::default();
 
-	for word in words {
-		if seen_set.contains(word) {
-			return Err(format!("password contains word {word} more than once"));
-		} else {
-			seen_set.insert(word);
+			for word in words {
+				if seen_set.contains(word) {
+					return Err(format!("password contains word {word} more than once"));
+				} else {
+					seen_set.insert(word);
+				}
+			}
+		}
+
+		SystemPolicy::PartTwo => {
+			let mut seen_set = BTreeSet::default();
+
+			for word in words {
+				let mut chars = word.chars().collect::<Vec<_>>();
+				chars.sort();
+				if seen_set.contains(&chars) {
+					return Err(format!(
+						"password contains anagrams of {} more than once",
+						chars.iter().collect::<String>()
+					));
+				} else {
+					seen_set.insert(chars);
+				}
+			}
 		}
 	}
 
 	Ok(())
 }
 
-fn password_is_valid(password: &str) -> bool {
-	validate_password(password).is_ok()
+fn password_is_valid(password: &str, policy: SystemPolicy) -> bool {
+	validate_password(password, policy).is_ok()
 }
 
 #[test]
-fn validate_password_correct() {
-	assert_eq!(Ok(()), validate_password("aa bb cc dd ee"));
+fn part_one_policy_correct() {
+	assert_eq!(
+		Ok(()),
+		validate_password("aa bb cc dd ee", SystemPolicy::PartOne)
+	);
 	assert_eq!(
 		Err(format!("password contains word aa more than once")),
-		validate_password("aa bb cc dd aa")
+		validate_password("aa bb cc dd aa", SystemPolicy::PartOne)
 	);
-	assert_eq!(Ok(()), validate_password("aa bb cc dd aaa"));
+	assert_eq!(
+		Ok(()),
+		validate_password("aa bb cc dd aaa", SystemPolicy::PartOne)
+	);
 }
 
+#[test]
+fn part_two_policy_correct() {
+	assert_eq!(
+		Ok(()),
+		validate_password("abcde fghij", SystemPolicy::PartTwo)
+	);
+	assert_eq!(
+		Ok(()),
+		validate_password("iiii oiii ooii oooi oooo", SystemPolicy::PartTwo)
+	);
+
+	assert_eq!(
+		Err("password contains anagrams of abcde more than once".to_string()),
+		validate_password("abcde xyz ecdab", SystemPolicy::PartTwo)
+	);
+	assert_eq!(
+		Ok(()),
+		validate_password("a ab abc abd abf abj", SystemPolicy::PartTwo)
+	);
+	assert_eq!(
+		Err("password contains anagrams of iiio more than once".to_string()),
+		validate_password("oiii ioii iioi iiio", SystemPolicy::PartTwo)
+	);
+}
 pub fn parse(input: &str) -> anyhow::Result<Intermediate> {
 	let passwords = input.lines().collect();
 	Ok(passwords)
@@ -42,11 +98,17 @@ pub fn part_one(passwords: &Intermediate) -> Option<Solution> {
 	Some(
 		passwords
 			.iter()
-			.filter(|password| password_is_valid(&password))
+			.filter(|password| password_is_valid(&password, SystemPolicy::PartOne))
 			.count(),
 	)
 }
 
-pub fn part_two(_intermediate: &Intermediate) -> Option<Solution> {
-	None
+pub fn part_two(passwords: &Intermediate) -> Option<Solution> {
+	Some(
+		passwords
+			.iter()
+			.filter(|password| password_is_valid(&password, SystemPolicy::PartOne))
+			.filter(|password| password_is_valid(&password, SystemPolicy::PartTwo))
+			.count(),
+	)
 }
