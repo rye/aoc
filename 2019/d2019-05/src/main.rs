b5a4c004dce8dd2767fc2832809ff8fc68836e46
Kristofer Rye <kristofer.rye@gmail.com>
2023-09-03 08:18:17 -0500

    2019/05: Migrate Part One to daocutil framework
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- 2019/d2019-05/src/main.rs
+++ 2019/d2019-05/src/main.rs
@@ -15,28 +15,6 @@ fn main() {
 		})
 		.collect();
 
-	// Part One: Compute diagnostic code
-	{
-		let program = program.clone();
-		let mut intcode: intcode::Intcode = intcode::Intcode::from(program);
-		intcode = intcode.input(1);
-		intcode = intcode.run();
-
-		let mut outputs: Vec<i32> = Vec::new();
-
-		loop {
-			if let Some(output) = intcode.output() {
-				outputs.push(output);
-			} else {
-				break;
-			}
-		}
-
-		println!("{:?}", outputs);
-
-		println!("Part One: {}", outputs.last().expect("expected an output"));
-	}
-
 	// Part Two: Compute diagnostic code for System ID 5
 	{
 		let program = program.clone();
