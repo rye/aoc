d0c6f8dcc4f031be28b7b5d28d11f2b1ad8d8b91
Kristofer Rye <kristofer.rye@gmail.com>
2024-04-07 13:19:15 -0500

    2016/02: Add failing test for day 2, part 1
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- d2016/src/day02.rs
+++ d2016/src/day02.rs
@@ -1,10 +1,18 @@
 pub type Intermediate = ();
-pub type Output = u32;
+pub type Output = String;
 
 /// # Errors
 pub fn parse(_data: &str) -> anyhow::Result<Intermediate> {
 	Ok(())
 }
+
+daocutil::test_example!(
+	part_one_example,
+	parse,
+	part_one,
+	include_str!("examples/day02"),
+	Some("1985".to_string())
+);
 
 #[must_use]
 pub fn part_one(_intermediate: &Intermediate) -> Option<Output> {
